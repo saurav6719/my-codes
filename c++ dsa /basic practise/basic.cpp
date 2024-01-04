e596b72ef11d115876a2720525128f3d22b203fe
Saurav Kumar <sauravkr485@gmail.com>
2024-01-04 20:26:51 +0530

    4 jan codeforces

--- c++ dsa /basic practise/basic.cpp
+++ c++ dsa /basic practise/basic.cpp
@@ -1,21 +1,10 @@
 #include<iostream>
 using namespace std;
-int help(int arr[], int n){
-    int ans = 1;
-    for(int i = 1; i<n;i++){
-        int j =0;
-        for(j = 0;j<i;j++){
-            if(arr[i] == arr[j]){
-                break;
-            }
-        }
-        if(i==j) ans++;
-    }
-    return ans;
-}
+
 int main(){
-    int arr[] ={1,2,3,4,3,2,1};
-    int n = sizeof(arr)/ sizeof(arr[0]);
-    cout<<help(arr,n); 
+    int n = -1;
+    bool flag = false;
+    flag = bool(n);
+    cout<<flag;
     return 0;
 }
