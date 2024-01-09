23ec948f7e9bf6e9e2948d0537feb172a262807b
Saurav Kumar <sauravkr485@gmail.com>
2024-01-09 18:28:16 +0530

    ...

---
+++ tle level 2/week 1 recursion/subsets2.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<climits>
+#include<cmath>
+#include<algorithm>
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c));
+#define mx(a,b,c) max(a,max(b,c));
+using namespace std;
+void solve(){ios_base::sync_with_stdio(false);cin.tie(NULL);
+    class Solution {
+public:
+void helper(vector<int>&nums, vector<vector<int> >&ans,int i, vector<int> sub){
+    if(i==nums.size()){
+        for(int a = 0; a< ans.size(); a++){
+            if(ans[a] == sub) return;
+        }
+        ans.push_back(sub);
+        return;
+    }
+    helper(nums,ans,i+1,sub);
+    sub.push_back(nums[i]);
+    helper(nums,ans,i+1,sub);
+    return;
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int> > ans;
+        vector<int> result;
+        helper(nums,ans,0,result);
+        return ans;
+    }
+};
+}
+int32_t main(){
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
