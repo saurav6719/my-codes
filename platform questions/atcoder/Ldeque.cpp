109b55ddac7b4a53485e294fb9527cef8b0a93e3
Saurav Kumar <sauravkr485@gmail.com>
2024-01-11 00:03:36 +0530

    Ldeque

--- platform questions/atcoder/Ldeque.cpp
+++ platform questions/atcoder/Ldeque.cpp
@@ -16,6 +16,19 @@ int f(vector<int> &input, int i, int j){
     if(dp[i][j] != -1) return dp[i][j];
     return dp[i][j] = max(input[i] - f(input,i+1,j) , input[j] - f(input,i,j-1));
 }
+int fbu(vector<int> &input, int n){
+    //base case
+    for(int i = 0; i<n; i++){
+        dp[i][i] = input[i];
+    }
+    for(int len = 2; len<=n ; len++){
+        for(int i = 0; i <= n-len;i++){
+            int j = i+ len -1;
+            dp[i][j] = max(input[i] - dp[i+1][j] , input[j] - dp[i][j-1]);
+        }
+    }
+    return dp[0][n-1];
+}
 void solve(){ios_base::sync_with_stdio(false);cin.tie(NULL);
     int n;
     cin>>n;
@@ -24,8 +37,8 @@ void solve(){ios_base::sync_with_stdio(false);cin.tie(NULL);
         cin >> input[i];
     }
     dp.clear();
-    dp.resize(3005,vector<int> (3005,-1));
-    cout<<f(input,0,n-1);
+    dp.resize(3005,vector<int> (3005,0));
+    cout<<fbu(input,n);
 
 }
 int32_t main(){
