951f7cf0dd1165748fa781e2f132030a67e9c3dc
Saurav Kumar <sauravkr485@gmail.com>
2024-01-14 23:31:51 +0530

    set or unset bit

---
+++ c++ dsa /Bit Manipulation/set_or_unset_bit.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<climits>
+#include<cmath>
+#include<algorithm>
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c));
+#define mx(a,b,c) max(a,max(b,c));
+using namespace std;
+
+string binary(int n){
+    bitset<8> b(n);
+    return b.to_string();
+}
+
+void solve(){ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int n = 35;
+    cout<<binary(n)<<endl;;
+    for(int i = 0; i<8; i++){
+        if(n & (1<<i)) cout<<"set"<<endl;
+        else cout<<"unset"<<endl;
+    }
+}
+int32_t main(){
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
