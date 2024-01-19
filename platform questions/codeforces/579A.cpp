fd3ed1e7f5fd1c0cb5686539e2d8a36b857f4125
Saurav Kumar <sauravkr485@gmail.com>
2024-01-19 19:45:53 +0530

    579 a

--- platform questions/codeforces/579A.cpp
+++ platform questions/codeforces/579A.cpp
@@ -4,22 +4,18 @@
 #include<climits>
 #include<cmath>
 #include<algorithm>
+#include<bitset>
 #define endl "\n"
 #define int long long int
 #define mod 1000000007
 #define mn(a,b,c) min(a,min(b,c));
 #define mx(a,b,c) max(a,max(b,c));
 using namespace std;
+
 void solve(){ios_base::sync_with_stdio(false);cin.tie(NULL);
-    int n = 536870910;
-    cout<<bitset<32>(n)<<endl;
-    // cout<<bitset<8>(n).count()<<endl;
-    // cout<<bitset<8>(n).flip()<<endl;
-    // cout<<bitset<8>(n).any()<<endl;
-    // cout<<bitset<8>(n).none()<<endl;
-    // cout<<bitset<8>(n).all()<<endl;
-    // string s = bitset<8>(n).to_string();
-    // cout<<s<<endl;
+    int x;
+    cin>>x;
+    cout<<bitset<32> (x).count();
 }
 int32_t main(){
     int t;
