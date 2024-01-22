b89b06955d89b9b601d4b1492bc5ad73b86161cb
Saurav Kumar <sauravkr485@gmail.com>
2024-01-23 00:14:23 +0530

    weird algorithm

---
+++ platform questions/cses problem set/wierd_algorithm.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<climits>
+#include<cmath>
+#include<algorithm>
+#include<unordered_map>
+#include<unordered_set>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#else
+#define debug(x)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+void helper(int n){
+    if(n==1) {
+        cout<<"1";
+        return;
+    }
+    if(n&1){
+        cout<<n<<" ";
+        helper(3*n+1);
+    }
+    else {
+        cout<<n<<" ";
+        helper(n/2);
+    }
+}
+void solve(){
+    int n;
+    cin>>n;
+    helper(n);
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
