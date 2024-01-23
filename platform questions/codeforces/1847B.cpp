9e08c25dac8cd55411ef27568223696b18a7cf91
Saurav Kumar <sauravkr485@gmail.com>
2024-01-23 21:47:02 +0530

    1847B coeforces

---
+++ platform questions/codeforces/1847B.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<climits>
+#include<cmath>
+#include<algorithm>
+#include<unordered_map>
+#include<unordered_set>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#else
+#define debug(x)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
+    }
+    int i = 0;
+    int ans = 0;
+    int sand = input[0];
+    while(i<n){ 
+        sand = sand & input[i];
+        if(sand == 0) {
+            ans++;
+            sand = input[i+1];
+        }
+        i++;
+    }
+    if(ans == 0) {
+        cout<<"1"<<endl;
+        return;
+    }
+    cout<<ans<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
