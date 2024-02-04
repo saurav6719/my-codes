36f939afe416d82cd1b4ac4081b0d136ebe1fd8f
Saurav Kumar <sauravkr485@gmail.com>
2024-02-05 00:47:13 +0530

    .

--- c++ dsa /Bit Manipulation/bitsetString.cpp
+++ c++ dsa /Bit Manipulation/bitsetString.cpp
@@ -13,19 +13,7 @@ using namespace std;
 void solve(){ios_base::sync_with_stdio(false);cin.tie(NULL);
     int n = 127929199446003072;
     cout<<bitset<64>(n)<<endl;
-    // cout<<bitset<8>(n).count()<<endl;
-    // cout<<bitset<8>(n).flip()<<endl;
-    // cout<<bitset<8>(n).any()<<endl;
-    // cout<<bitset<8>(n).none()<<endl;
-    // cout<<bitset<8>(n).all()<<endl;
-    // string s = bitset<8>(n).to_string();
-    // cout<<s<<endl;
-    0000001111101111110010010011101000111101010110100110100110000110
-    0000000000000011000010000110110001011010111000110110110000101010
-    0000000111000000000000000000000000000000000000000000000000000000
-
-    0000000111000110011111101110110111011001011000010000110110000000
-
+    
 }
 int32_t main(){
     int t;
