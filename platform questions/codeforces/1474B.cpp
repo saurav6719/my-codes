2be0b82f250e74224a2ee18e9f767d6401a7aefb
Saurav Kumar <sauravkr485@gmail.com>
2024-02-04 15:30:58 +0530

    different divisors

--- platform questions/codeforces/1474B.cpp
+++ platform questions/codeforces/1474B.cpp
@@ -17,7 +17,34 @@
 #define mn(a,b,c) min(a,min(b,c))
 #define mx(a,b,c) max(a,max(b,c))
 using namespace std;
+
+
 void solve(){
+    vector<bool> sieve(100000, 1); // 1 represents prime
+    sieve[0] = 0;
+    sieve[1] = 0;
+
+    for(int i = 2; i * i<=100000; i++){
+        if(sieve[i] == 0) continue;
+        for(int j = i * i; j<=100000; j+=i){    // complexity O(n log(log n)) for n queries 
+            sieve[j] = 0;
+        }
+    }
+    int d;
+    cin>>d;
+    int ans = 1;
+    int count = 0;
+    int first = 1+d;
+    for(int i = first; i<=100000 and count<2; ){
+        if(sieve[i] == true){
+            ans *= i;
+            count++;
+            i+=d;
+        }
+        else i++;
+    }
+
+    cout<<ans<<endl;
     
 }
 int32_t main(){
@@ -27,8 +54,8 @@ int32_t main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
-    t = 1;
+    cin>>t;
+    //t = 1;
     while(t--){
         solve();
     }
