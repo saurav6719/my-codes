dc0be2b52e3fbb80ccd186016ef0d03cff56988b
Saurav Kumar <sauravkr485@gmail.com>
2024-02-09 15:54:45 +0530

    cses grid path introductory

--- platform questions/cses problem set/grid_path_introductory.cpp
+++ platform questions/cses problem set/grid_path_introductory.cpp
@@ -17,51 +17,124 @@
 #define mn(a,b,c) min(a,min(b,c))
 #define mx(a,b,c) max(a,max(b,c))
 using namespace std;
-int ans = 0;
-unordered_set<string> vis;
-bool cango(string &input, int i, int j, int count){
-    debug(i);
-    debug(j);
-    debug(count);
-    string str ="";
-    str+= to_string(i);
-    str+= '>';
-    str+= to_string(j);
-    debug(str);
-    if(vis.count(str)) return false;
-    if(count!=48 and i==6 and j==0) return false; 
-    if(i<0 or j<0 or i==7 or j==7) return false;
-    if(input[count] == '?') return true;
-}
-void f(string &input, int i, int j, int count){
-    if(count == 48 and i==6 and j==0) {
-        ans++;
+const int n = 7;
+const int total_steps = 48;
+int reserved[49];
+bool visited [n][n];
+string path;
+void move(int row, int col, int &ans , int &steps){
+    if(row == n-1 and col == 0){
+        if(steps == total_steps) ans++;
         return;
     }
-    string str ="";
-    str+= to_string(i);
-    str+= '>';
-    str+= to_string(j);
-    vis.insert(str);
-    if(cango(input,i+1,j,count+1)) f(input,i+1,j,count+1);
-    if(cango(input,i,j+1,count+1)) f(input,i,j+1,count+1);
-    if(cango(input,i-1,j,count+1)) f(input,i-1,j,count+1);
-    if(cango(input,i,j-1,count+1)) f(input,i,j-1,count+1);
-    debug(i);
-    debug(j);
-    debug(count);
-    if(input[count+1] == 'R') f(input,i,j+1,count+1);
-    if(input[count+1] == 'L') f(input,i,j-1,count+1);
-    if(input[count+1] == 'U') f(input,i-1,j,count+1);
-    if(input[count+1] == 'D') f(input,i+1,j,count+1);
-    vis.erase(str);
-}
+    // wall or left right unvisited up down visited
+    if (((row + 1 == n || (visited[row - 1][col] && visited[row + 1][col])) && col - 1 >= 0 && col + 1 < n && !visited[row][col - 1] && !visited[row][col + 1])){
+        return ;
+    }
+    if(((col + 1 == n || (visited[row][col - 1] && visited[row][col + 1])) && row - 1 >= 0 && row + 1 < n && !visited[row - 1][col] && !visited[row + 1][col])){
+        return ;
+    }
+    if(((row == 0 || (visited[row + 1][col] && visited[row - 1][col])) && col - 1 >= 0 && col + 1 < n && !visited[row][col - 1] && !visited[row][col + 1])){
+        return ;
+    }
+    if(((col == 0 || (visited[row][col + 1] && visited[row][col - 1])) && row - 1 >= 0 && row + 1 < n && !visited[row - 1][col] && !visited[row + 1][col])){
+        return;
+    }
 
+    visited[row][col] = true;
+    if(path[steps] != '?'){
+        if(path[steps] == 'U'){
+            if(row - 1>= 0){
+                if(!visited[row-1][col]){
+                    steps++;
+                    move(row-1,col,ans,steps);
+                    steps--;
+                }
+            }
+        }
+        else if(path[steps] == 'R'){
+            if(col + 1 < n){
+                if(!visited[row][col+1]){
+                    steps++;
+                    move(row,col+1,ans,steps);
+                    steps--;
+                }
+            }
+        }
+        else if(path[steps] == 'D'){
+            if(row + 1 < n){
+                if(!visited[row+1][col]){
+                    steps++;
+                    move(row+1,col,ans,steps);
+                    steps--;
+                }
+            }
+        }
+        else if(path[steps] == 'L'){
+            if(col - 1>= 0){
+                if(!visited[row][col-1]){
+                    steps++;
+                    move(row,col-1,ans,steps);
+                    steps--;
+                }
+            }
+        }
+    }
+    else{
+        // move down
+        if (row + 1 < n){
+            if (!visited[row + 1][col])
+            {
+                steps++;
+                move(row + 1, col, ans, steps);
+                steps--;
+            }
+        }
+        // move right
+        if (col + 1 < n){
+            if (!visited[row][col + 1])
+            {
+                steps++;
+                move(row, col + 1, ans, steps);
+                steps--;
+            }
+        }
+        // move up
+        if (row - 1 >= 0){
+            if (!visited[row - 1][col])
+            {
+                steps++;
+                move(row - 1, col, ans, steps);
+                steps--;
+            }
+        }
+        // move left
+        if (col - 1 >= 0)
+        {
+            if (!visited[row][col - 1])
+            {
+                steps++;
+                move(row, col - 1, ans, steps);
+                steps--;
+            }
+        }
+    }
+    visited[row][col] = false;
+}
 void solve(){
-    string str;
-    cin>>str;
-    f(str,0,0,0);
-    cout<<ans;
+    cin>>path;
+    for(int i = 0; i<path.length(); i++){
+        if(path[i] == '?') reserved[i] = -1;
+        else if(path[i] == 'U') reserved[i] = 0;
+        else if(path[i] == 'R') reserved[i] = 1;
+        else if(path[i] == 'D') reserved[i] = 2;
+        else if(path[i] == 'L') reserved[i] = 3;
+    }
+    int ans = 0;
+    int steps = 0;
+    move(0,0,ans,steps);
+    cout<<ans<<endl;
+
 }
 int32_t main(){
     ios_base::sync_with_stdio(false);
