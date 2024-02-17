f9816881d0621c180841c54897313e752a02096c
Saurav Kumar <sauravkr485@gmail.com>
2024-02-18 02:38:07 +0530

    ...

--- contest/codeforces/924 div 2/C.cpp
+++ contest/codeforces/924 div 2/C.cpp
@@ -17,8 +17,47 @@
 #define mn(a,b,c) min(a,min(b,c))
 #define mx(a,b,c) max(a,max(b,c))
 using namespace std;
+void factors(vector<int> & v, int n){
+    for(int i = 1; i*i <=n ; i++){
+        if(n % i ==0) {
+            if(i*i == n) v.push_back(i);
+            else {
+                v.push_back(i);
+                v.push_back(n/i);
+            }
+        }
+    }
+}
 void solve(){
+    int n;
+    int x;
+    cin>>n>>x;
+    int ans = 0;
+    vector<int> v;
+    int a = n-x;
+    int b = n+x-2;
+    factors(v,a);
+    unordered_set<int> st;
+    for(auto ele : v){
+        if(ele % 2 == 0){
+            int k = (ele+2)/ 2;
+            if(k>=x) {
+                st.insert(k);
+            }
+        }
+    }
+    vector<int> v1;
+    factors(v1,b);
+    for(auto ele : v1){
+        if(ele % 2 == 0){
+            int k = (ele+2)/ 2;
+            if(k>=x) {
+                st.insert(k);
+            }
+        }
+    }
     
+    cout<<st.size()<<endl;
 }
 int32_t main(){
     ios_base::sync_with_stdio(false);
@@ -27,8 +66,8 @@ int32_t main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
-    t = 1;
+    cin>>t;
+    //t = 1;
     while(t--){
         solve();
     }
