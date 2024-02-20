cb136265626bbecb252f08463dae0c112cb2c8bd
Saurav Kumar <sauravkr485@gmail.com>
2024-02-21 00:14:34 +0530

    daf

--- try.cpp
+++ try.cpp
@@ -18,32 +18,8 @@
 #define mx(a,b,c) max(a,max(b,c))
 using namespace std;
 void solve(){
-    int n;
-    cin>>n;
-    string input;
-    cin>>input;
-    int ans = 0;
-    int brak = 0;
-    bool check = false;
-    for(int i = 0; i<n-1; i++){
-        if(input[i] == '*' and input[i+1] == '*'){
-            brak = i;
-            check = true;
-            break;
-        }
-    }
-    debug(brak);
-    if(check == false){
-        for(int i = 0; i<n; i++){
-        if(input[i] == '@') ans++;
-        }
-    }
-    else{
-        for(int i = 0; i<brak;i++){
-            if(input[i] == '@') ans++;
-        }
-    }
-    cout<<ans<<endl;
+    vector<vector<int> > input;
+    cout<<"lgkva";
 }
 int32_t main(){
     ios_base::sync_with_stdio(false);
@@ -52,8 +28,8 @@ int32_t main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    cin>>t;
-    //t = 1;
+    //cin>>t;
+    t = 1;
     while(t--){
         solve();
     }
