3cda45f16ee669fcea9a12d51e3a41758b3bb2e5
Saurav Kumar <sauravkr485@gmail.com>
2024-02-25 17:23:05 +0530

    c.

--- contest/codeforces/edu 162/c.cpp
+++ contest/codeforces/edu 162/c.cpp
@@ -1,100 +1,74 @@
-#include <iostream>
-#include <vector>
-
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#else
+#define debug(x)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
 using namespace std;
-
-// Structure to represent a node in the segment tree
-struct Node {
-    int frequency;
-    int element;
-};
-
-// Function to build the segment tree
-void buildSegmentTree(vector<Node>& segmentTree, const vector<int>& arr, int low, int high, int pos) {
-    if (low == high) {
-        segmentTree[pos].frequency = 1;
-        segmentTree[pos].element = arr[low];
-        return;
-    }
-
-    int mid = (low + high) / 2;
-    buildSegmentTree(segmentTree, arr, low, mid, 2 * pos + 1);
-    buildSegmentTree(segmentTree, arr, mid + 1, high, 2 * pos + 2);
-
-    if (segmentTree[2 * pos + 1].frequency >= segmentTree[2 * pos + 2].frequency) {
-        segmentTree[pos] = segmentTree[2 * pos + 1];
-    } else {
-        segmentTree[pos] = segmentTree[2 * pos + 2];
-    }
-}
-
-// Function to query the segment tree for maximum frequency in a range
-Node querySegmentTree(const vector<Node>& segmentTree, int qlow, int qhigh, int low, int high, int pos) {
-    if (qlow > high || qhigh < low) {
-        Node nullNode = {0, -1};
-        return nullNode;
-    }
-
-    if (qlow <= low && qhigh >= high) {
-        return segmentTree[pos];
-    }
-
-    int mid = (low + high) / 2;
-    Node left = querySegmentTree(segmentTree, qlow, qhigh, low, mid, 2 * pos + 1);
-    Node right = querySegmentTree(segmentTree, qlow, qhigh, mid + 1, high, 2 * pos + 2);
-
-    if (left.frequency >= right.frequency) {
-        return left;
-    } else {
-        return right;
-    }
-}
-
-int findMaxOccurringElement(const vector<int>& arr, const vector<pair<int, int> >& queries) {
-    int n = arr.size();
-
-    // Build the segment tree
-    vector<Node> segmentTree(4 * n);
-    buildSegmentTree(segmentTree, arr, 0, n - 1, 0);
-
-    int maxElement = -1;
-    int maxFrequency = 0;
-
-    for (const auto& query : queries) {
-        int qlow = query.first - 1;
-        int qhigh = query.second - 1;
-
-        Node result = querySegmentTree(segmentTree, qlow, qhigh, 0, n - 1, 0);
-
-        if (result.frequency > maxFrequency) {
-            maxFrequency = result.frequency;
-            maxElement = result.element;
-        }
-    }
-
-    return maxFrequency;
-}
-
-int main() {
-    // Example usage with multiple queries
+void solve(){
     int n,q;
     cin>>n>>q;
-    vector<int> input(n);
-    for(int i = 0; i<n; i++){
+    vector<int> input(n+1);
+    for(int i = 1; i<=n; i++){
         cin>>input[i];
     }
-    vector<pair<int,int> >queries;
+    vector<int> oc(n+1,0);
+    vector<int> sumc(n+1,0);
+    for(int i = 1; i<=n; i++){
+        if(input[i] == 1) {
+            oc[i] = oc[i-1]+1;
+            sumc[i] = sumc[i-1];
+        }
+        else{
+            oc[i] = oc[i-1];
+            sumc[i] = sumc[i-1]+input[i]-1;
+        }
+    }
+    // for(int i = 0; i<=n; i++){
+    //     cout<<oc[i]<<" ";
+    // }
+    // cout<<endl;
+    // for(int i = 0; i<=n; i++){
+    //     cout<<sumc[i]<<" ";
+    // }
+    cout<<endl;
     while(q--){
         int l,r;
         cin>>l>>r;
-        pair<int,int> pp;
-        pp.first = l;
-        pp.second = r;
-        queries.push_back(pp);
-        int result = findMaxOccurringElement(input, queries);
-        cout << "The element occurring with the maximum frequency in each query is: " << result << endl;
+        if(l==r){
+            cout<<"NO"<<endl;
+            continue;
+        }
+        int x = sumc[r]- sumc[l-1];
+        debug(sumc[r]);
+        debug(sumc[l]);
+        debug(oc[r]);
+        debug(oc[l]);
+        debug(x);
+        int y = oc[r] - oc[l-1];
+        debug(y);
+        if(x<y) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
     }
 
 
-    return 0;
+}
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
 }
