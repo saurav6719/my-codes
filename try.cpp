abadd9b2123ea5cac6404e125f3bec5cb08c7809
Saurav Kumar <sauravkr485@gmail.com>
2024-02-26 15:59:32 +0530

    ....

--- try.cpp
+++ try.cpp
@@ -1,8 +1,16 @@
 #include<bits/stdc++.h>
 #ifndef ONLINE_JUDGE
 #define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
 #else
 #define debug(x)
+#define print(v)
 #endif
 #define endl "\n"
 #define int long long int
@@ -11,8 +19,13 @@
 #define mx(a,b,c) max(a,max(b,c))
 using namespace std;
 void solve(){
-    vector<int> v = {1,2, 3,4,5};
-    cout<<v[0];
+    int n;
+    cin>>n;
+    string str;
+    cin>>str;
+   vector<int> v = {1,2,3,4,5};
+   print(v);
+   cout<<0;
 }
 signed main(){
     ios_base::sync_with_stdio(false);
@@ -21,8 +34,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
-    t = 1;
+    cin>>t;
+    //t = 1;
     while(t--){
         solve();
     }
