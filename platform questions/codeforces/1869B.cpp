e57413de85167926241b41ddf1146f241f6c7bfc
Saurav Kumar <sauravkr485@gmail.com>
2024-03-22 12:22:07 +0530

    1869B

--- platform questions/codeforces/1869B.cpp
+++ platform questions/codeforces/1869B.cpp
@@ -38,7 +38,40 @@ using namespace std;
 
 /* write core logic here */
 void solve(){
+    int n,k,a,b;
+    cin>>n>>k>>a>>b;
+    vector<pair<int,int> > input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i].first>>input[i].second;
+    }
+    int direct = abs(input[a-1].first - input[b-1].first) + abs(input[a-1].second - input[b-1].second);
+    if(k==0) {
+        cout<<direct<<endl;
+        return;
+    }
+
+    int a_close = abs(input[a-1].first - input[0].first) + abs(input[a-1].second - input[0].second);
+    int idx_aclose = -1;
+    int b_close = abs(input[b-1].first - input[0].first) + abs(input[b-1].second - input[0].second);
+    int idx_bclose = -1;
+    for(int i = 0; i<k; i++){
+        int aa = abs(input[a-1].first - input[i].first)+abs(input[a-1].second - input[i].second);
+        if(aa < a_close) {
+            a_close = aa;
+            idx_aclose = i;
+        }
+        int bb = abs(input[b-1].first - input[i].first)+abs(input[b-1].second - input[i].second);
+        debug(bb);
+        if(bb < b_close) {
+            b_close = bb;
+            idx_bclose = i;
+        }
+    }
     
+    debug(a_close);
+    debug(b_close);
+    debug(direct);
+    cout<<min(direct , a_close + b_close)<<endl;
 }
 /* logic ends */
 
@@ -49,8 +82,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
-    t = 1;
+    cin>>t;
+    //t = 1;
     while(t--){
         solve();
     }
