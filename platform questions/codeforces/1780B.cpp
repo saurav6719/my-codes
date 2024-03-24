867784a07a03ab28e31903eb562d0ff46090a42a
Saurav Kumar <sauravkr485@gmail.com>
2024-03-24 16:54:59 +0530

    1780 b

---
+++ platform questions/codeforces/1780B.cpp
@@ -0,0 +1,89 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
+    }
+    vector<int> prf(n);
+    prf[0] = input[0];
+    for(int i = 1; i<n; i++){
+        prf[i] = prf[i-1] + input[i];
+    }
+    print(prf);
+    vector<int> suf(n);
+    suf[n-1] = input[n-1];
+    for(int i= n-2; i>=0; i--){
+        suf[i] = suf[i+1] + input[i];
+    }
+    print(suf);
+    int res = INT_MIN;
+    for(int i = 0; i<n-1; i++){
+        res = max(res, gcd(prf[i], suf[i+1]));
+    }
+    cout<<res<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
