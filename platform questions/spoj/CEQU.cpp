53c745185377586d6af3b466f061cd5ddd607b11
Saurav Kumar <sauravkr485@gmail.com>
2024-04-09 20:47:38 +0530

    cequ spoj

---
+++ platform questions/spoj/CEQU.cpp
@@ -0,0 +1,75 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+/* write core logic here */
+void solve(){
+    
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    for(int i = 1; i<=t; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        int x = gcd(a,b);
+        if(c%x == 0) {
+            cout<<"Case "<<i<<": Yes"<<endl;
+        }
+
+        else cout<<"Case "<<i<<": No"<<endl;
+    }
+return 0;
+}
+
