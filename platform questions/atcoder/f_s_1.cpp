8b5ca0b13c81ebfdcf42fa1e6f617707d3570e3f
Saurav Kumar <sauravkr485@gmail.com>
2024-04-10 14:09:34 +0530

    atcoder fs

---
+++ platform questions/atcoder/f_s_1.cpp
@@ -0,0 +1,110 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+       
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+   
+    return d;
+}
+
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g = gcd(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
+
+
+
+
+/* write core logic here */
+void solve(){
+    int x,y;
+    cin>>x>>y;
+    //ax+by = c;
+    //xb-ya = 2;
+    //xb-ya = -2;
+    if( 2 % gcd(x,-1*y) != 0) {
+        cout<<-1<<endl;
+        return;
+    }
+    int g = gcd(x,-1*y);
+    int one;
+    int two;
+    if(find_any_solution(x,-1*y,2,one,two,g)){
+        cout<<two<<" "<<one<<endl;
+    }
+    else{
+        if(find_any_solution(x,-1*y,-2,one,two,g)){
+            cout<<two<<" "<<one<<endl;
+        }
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
