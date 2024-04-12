58dfbf09ed0edda90074e1a8bdb9220559044aa0
Saurav Kumar <sauravkr485@gmail.com>
2024-04-12 20:28:09 +0530

    ..

--- platform questions/cses problem set/arrayDescription.cpp
+++ platform questions/cses problem set/arrayDescription.cpp
@@ -38,21 +38,46 @@ using namespace std;
 
 /* write core logic here */
 void solve(){
-    int n,m,k;
-    cin>>n>>m>>k;
-
-    int maxsame = n/m;
-    if(n%m != 0 ){
-        maxsame++;
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+    vector<int> input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
     }
-    int remain = n- maxsame;
-    //debug(remain);
-    if(k<remain){
-        cout<<"YES"<<endl;
-        return;
-    }
-    cout<<"NO"<<endl;
 
+    vector<vector<int> >dp;
+    dp.resize(n+1, vector<int> (m+1, 0));
+    //bse case
+
+    for(int i = 1; i<=m; i++){
+        if(input[0] != 0 and input[0] != i){
+            dp[0][i] = 0;
+        }
+        else{
+            dp[0][i] = 1;
+        }
+    }
+
+    for(int i = 1; i<n; i++){
+        for(int j = 1; j<=m; j++){
+            if(input[i] != 0 and input[i] != j){
+                dp[i][j] = 0;
+                continue;
+            }
+            dp[i][j] = (((dp[i-1][j] % mod) + (dp[i-1][j-1]%mod)) % mod + (dp[i-1][j+1]%mod)) % mod;
+        }
+    }
+
+    int ans = 0;
+    for(int i = 1; i<=m; i++){
+        ans = ((ans %mod) + (dp[n-1][i]%mod)) % mod;
+    }
+    // print(dp[0]);
+    // print(dp[1]);
+    // print(dp[2]);
+    cout<<ans;
 
 }
 /* logic ends */
@@ -64,8 +89,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    cin>>t;
-    //t = 1;
+    //cin>>t;
+    t = 1;
     while(t--){
         solve();
     }
