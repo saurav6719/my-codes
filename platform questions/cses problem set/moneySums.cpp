7fce779059165a6b572d69eee4bc5bb69d6f0703
Saurav Kumar <sauravkr485@gmail.com>
2024-04-15 16:24:02 +0530

    money sum cses

---
+++ platform questions/cses problem set/moneySums.cpp
@@ -0,0 +1,88 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
+    }
+
+    vector<int> dp;
+    dp.push_back(0);
+    set<int> st;
+    st.insert(0);
+    for(int i = 0 ; i<n; i++){
+        int sz = dp.size();
+        for(int j = 0 ; j<sz; j++){
+            int newi = input[i] + dp[j];
+            //debug(newi);
+            if(st.count(newi)){
+                continue;
+            }
+            dp.push_back(newi);
+            st.insert(input[i] + dp[j]);
+        }
+    }
+
+
+    cout<<dp.size()- 1<<endl;
+    sort(dp.begin(), dp.end());
+    for(int i = 1; i<dp.size(); i++){
+        cout<<dp[i]<<" ";
+    }
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
