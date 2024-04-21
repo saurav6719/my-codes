8219b911872fb9014b835fa990ad6096c9974133
Saurav Kumar <sauravkr485@gmail.com>
2024-04-21 16:23:57 +0530

    1669H

---
+++ platform questions/codeforces/1669H.cpp
@@ -0,0 +1,96 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    vector<int> input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
+    }
+    vector<int> bits(31,0);
+    for(int i = 0 ; i<n; i++){
+        for(int j = 0 ; j<=30 ; j++){
+            if((input[i] & (1<<j)) == 0){
+                bits[j]++;
+            }
+        }
+    }
+    print(bits);
+    int j = 30;
+    int orr = 0;
+    while(k>0 and j>=0){
+        int req = bits[j];
+        if(k>= req) {
+            orr += (1<<j);
+            k-= req;
+        }
+        j--;  
+    }
+
+    int ans = input[0];
+    for(int i = 1; i<n; i++){
+        ans = ans & input[i];
+    }
+
+    ans = ans | orr;
+    cout<<ans<<endl;
+
+
+
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
