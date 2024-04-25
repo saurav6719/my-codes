ba6046a64c1ee664c4d7226875263998bee36cbb
Saurav Kumar <sauravkr485@gmail.com>
2024-04-26 02:20:45 +0530

    1459B

---
+++ platform questions/codeforces/1459B.cpp
@@ -0,0 +1,83 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+void solve(){
+    int n;
+    cin>>n;
+    if(n==1){
+        cout<<4<<endl;
+        return;
+    }
+    if(n==2){
+        cout<<4<<endl;
+        return;
+    }
+    int sum = 0;
+    if(n%2 == 0){
+       cout<<((n/2)+1)*((n/2)+1)<<endl;
+       return;
+    }
+    else{
+        // n is odd
+        int i = 2;
+        while(i<=n+1){
+            sum += i;
+            i+=2;
+        }
+        sum *= 2;
+    }
+    cout<<sum<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
