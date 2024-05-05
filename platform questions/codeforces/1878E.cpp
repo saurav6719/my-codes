dcf83e318589925c20e38abb7d246a83a0e7f7f4
Saurav Kumar <sauravkr485@gmail.com>
2024-05-06 01:55:26 +0530

    maa kaa bhosda question

--- platform questions/codeforces/1878E.cpp
+++ platform questions/codeforces/1878E.cpp
@@ -1,143 +1,65 @@
-/*
-  ------------------------------------------
- |                                        |
- |      Code Crafted by Saurav     |
- |                                        |
-  ------------------------------------------
-    \        ,     ,        /
-      \      |     |      /
-         \   \___/   /
-           \  -----  /
-             \_____/
-  
-  Happy coding! 
-*/
-
-/* includes and all */
-
-#include<bits/stdc++.h>
-#ifndef ONLINE_JUDGE
-#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
-#define print(v) do { \
-                    cout << "vect--" << #v << " = [ "; \
-                    for (int i = 0; i < v.size(); i++) { \
-                        cout << v[i] << " "; \
-                    } \
-                    cout << " ]" << endl; \
-                } while(0)
-#else
-#define debug(x)
-#define print(v)
-#endif
-#define endl "\n"
-#define int long long int
-#define mod 1000000007
-#define mn(a,b,c) min(a,min(b,c))
-#define mx(a,b,c) max(a,max(b,c))
+#include <bits/stdc++.h>
 using namespace std;
-
-/* write core logic here */
+using ll = long long;
+ 
+ 
+const int N =200003;
+const int bits=30;
+int pref[N][bits];
+int a[N];
+void Buildprefix(int n){ //Builds the prefix sums for each bit
+    for(int i=0; i< n; i++){
+        for(int j=0; j<30; j++){
+            if(a[i]&(1<<j)){
+                pref[i+1][j]=pref[i][j]+1;
+            }
+            else{
+                pref[i+1][j]=pref[i][j];
+            }
+        }
+    }
+}
 void solve(){
     int n;
-    cin>>n;
-    vector<int> input(n);
-    vector<vector<int> >bits(31, vector<int> ());
-
-    for(int i = 0; i<n; i++){
-        int ele;
-        cin>>ele;
-        input[i] = ele;
-        for(int j= 0; j<=30; j++){
-            if((ele & (1<<j)) == 0){
-                bits[j].push_back(i);
-            }
-        }
+    cin >> n;
+    for(int i=0; i< n; i++){
+        cin >> a[i];
     }
-
-    // print(bits[0]);
-    // print(bits[1]);
-    // print(bits[2]);
-    // print(bits[3]);
-    // print(bits[4]);
+    Buildprefix(n);
     int q;
-    cin>>q;
+    cin >> q;
     while(q--){
-        int l;
-        int k;
-        cin>>l;
-        cin>>k;
-        int res1 = -1;
-        int res2 = n;
-        int maxofk = -1;
-        for(int i = 30 ; i>=0; i--){
-            if((1<<i) & k){
-                maxofk = i;
-                break;
-            }
-        }
-
-        // debug(res2);
-        //debug(maxofk);
-        bool tobreak = false;
-        for(int i = maxofk+1;i<=30; i++){
-            if(input[l-1] &(1<<i)){
-                int xx = lower_bound(bits[i].begin(), bits[i].end(), l-1) - bits[i].begin();
-                if(xx == bits[i].size()){
-                    tobreak = true;
-                    break;
-                }
-                res1 = max(res1 , bits[i][xx]-1);
-            }
-        }
-
-        //debug(res1);
-        if(tobreak) {
-            cout<<n<<" ";
+        int l, k;
+        cin >> l >> k;
+        if(a[l-1]<k){
+            cout << -1 << '\n';
             continue;
         }
-
-        int num = 0;
-
-        for(int i = 0; i<=maxofk; i++){
-            // debug(((1<<i) & input[l-1]));
-            // debug(((1<<i) & k));
-            if((((1<<i) & input[l-1]) == 0)) continue;
-            int xx = lower_bound(bits[i].begin(), bits[i].end(), l-1) - bits[i].begin();
-            if(xx == bits[i].size()){
-                res2 = min(res2,n-1);
+        int lo=l;
+        int hi=n;
+        int ans=l;
+        while(lo<=hi){
+            int s=(lo+hi)/2;
+            int num=0;
+            for(int j=0; j< bits; j++){
+                if(pref[s][j]-pref[l-1][j]==s-l+1){
+                    num+=(1<<j);
+                }
             }
-            else res2 = min(res2 , bits[i][xx]-1);
-            // debug(i);
-            // debug(res2);
+            if(num>=k){
+                lo=s+1;
+                ans=max(ans, s);
+            }
+            else hi=s-1;
         }
-        for(int i = 0; i<=maxofk; i++){
-            if((1<<i) & input[l-1]) num += (1<<i);
-            if(num < k) res2 = -1;
-        }
-        //debug(res2);
-        // debug(res1);
-        // debug(res2);
-        int ans = max(res1,res2);
-        ans++;
-        if(ans == 0) ans--;
-        cout<<ans<<" ";
+        cout << ans << '\n';
     }
-    cout<<endl;
 }
-/* logic ends */
-
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    #ifndef ONLINE_JUDGE
-        freopen("Error.txt" , "w" , stderr);
-    #endif
-    int t;
-    cin>>t;
-    //t = 1;
+ 
+int main(){
+    int t = 1;
+    cin >> t;
     while(t--){
         solve();
     }
-return 0;
 }
-
