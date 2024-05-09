db3a14a3872ec609cd6ddc9e3a797894736c3b2f
Saurav Kumar <sauravkr485@gmail.com>
2024-05-09 06:47:39 +0530

    1513B.cpp

---
+++ platform questions/codeforces/1513B.cpp
@@ -0,0 +1,116 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+using namespace std;
+
+/* write core logic here */
+const int MOD = 1e9 + 7;
+
+// Function to calculate power in modular arithmetic
+long long power(long long x, long long y) {
+    long long res = 1;
+    x = x % MOD;
+    while (y > 0) {
+        if (y & 1)
+            res = (res * x) % MOD;
+        y = y >> 1; // y = y/2
+        x = (x * x) % MOD;
+    }
+    return res;
+}
+
+// Function to calculate factorial in modular arithmetic
+long long factorial(int n) {
+    long long res = 1;
+    for (int i = 2; i <= n; ++i)
+        res = (res * i) % MOD;
+    return res;
+}
+
+// Function to calculate nCr in modular arithmetic
+long long nCr(int n, int r) {
+    if (r == 0 || r == n)
+        return 1;
+    long long numerator = factorial(n);
+    long long denominator = (factorial(r) * factorial(n - r)) % MOD;
+    return (numerator * power(denominator, MOD - 2)) % MOD;
+}
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> input(n);
+    map<int,int> mp;
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
+        mp[input[i]]++;
+    }
+    int annd = input[0];
+    for(int i = 1; i<n; i++){
+        annd &= input[i];
+    }
+
+    if(mp[annd] <2){
+        cout<<0<<endl;
+        return;
+    }
+
+    int total = mp[annd];
+
+    int ans = 1;
+    ans *= 2;
+    ans %= mod;
+    ans *= factorial(n-2);
+    ans *= nCr(total,2);
+    ans %= mod;
+    cout<<ans<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
