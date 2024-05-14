4b212102acfa213635b3ba18d4f4d86f0a061892
Saurav Kumar <sauravkr485@gmail.com>
2024-05-14 09:34:25 +0530

    1167 D

--- platform questions/codeforces/1167D.cpp
+++ platform questions/codeforces/1167D.cpp
@@ -37,43 +37,8 @@
 using namespace std;
 
 /* write core logic here */
-
-vector<list<int> > graph;
-
-int v;
-
-void add_edge(int src,int dest, bool directed = false){
-    graph[src].push_back(dest);
-    if(!directed){
-        graph[dest].push_back(src);
-    }
-}
-
-
 void solve(){
-    cin>>v;
-    graph.resize(v,list<int> ());
-    int e;
-    cin>>e;
-    while(e--){
-        int num;
-        cin>>num;
-        if(num==0) continue;
-        int ff;
-        cin>>ff;
-        if(num == 1) continue;
-        int dest;
-        cin>>dest;
-        add_edge(ff,dest);
-    }
-
-    int ans = 0;
-    vector<int> res(n+5,-1);
-    for(int i = 1; i<=n; i++){
-
-    }
     
-
 }
 /* logic ends */
 
