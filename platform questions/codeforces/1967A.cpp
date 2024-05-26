e5da42bf575df605ea108c7418e9b1b50f29af44
Saurav Kumar <sauravkr485@gmail.com>
2024-05-26 10:18:43 +0530

    1967A

--- platform questions/codeforces/1967A.cpp
+++ platform questions/codeforces/1967A.cpp
@@ -43,82 +43,83 @@ void solve(){
     int k;
     cin>>k;
     vector<int> input(n);
-    map<int,int> mp;
+
     for(int i = 0; i<n; i++){
         cin>>input[i];
-        mp[input[i]]++;
-    }
-    for(auto ele : mp){
-        cout<<ele.first<<" "<<ele.second<<endl;
     }
 
-    while(k>0){
-        if(mp.size() <= 1) break;
-        auto it = mp.begin();
-        pair<int,int> pp = *it;
-        mp.erase(it);
-        auto it2 = mp.begin();
-        pair<int,int> pp2 = *it2;
-        mp.erase(it2);
-        auto diff = pp2.first - pp.first;
-        auto multi = pp.second;
-        int resf = pp.second + pp2.second;
-        diff *= multi;
-        debug(diff);
-        if(k>=diff){
-            // debug(pp2.first);
-            // debug(pp2.second);
-            k-= diff;
-            if(resf>0) mp.insert({pp2.first, resf});
-        }
-        
-        else{
-            //debug(pp.first);
-
-            //debug(pp.second);
-
-            if(k>0) mp.insert({pp.first +k, k});
-            if(pp.second-k > 0)mp.insert({pp.first, pp.second-k});
-            if(mp.count(pp2.first)){
-                mp[pp2.first] += pp2.second;
-            }
-            else mp.insert({pp2.first, pp2.second});            
-            k = 0;
-            break;
-        }
-
-    }
-    debug(k);
-    if(k>0){
-        auto it = mp.begin();
-        mp.erase(it);
-        pair<int,int> pp = *it;
-        int xx = pp.first;
-        int yy = pp.second;
-        
-        xx += k/yy;
-        mp.insert({xx,yy});
+    if(n==1){
+        cout<<input[0]+k<<endl;
+        return;
     }
 
+    sort(input.begin(), input.end());
 
-    cout<<endl;
-    for(auto ele : mp){
-        cout<<ele.first<<" "<<ele.second<<endl;
+    vector<int> diff;
+    for(int i = 1; i<n; i++){
+        diff.push_back(input[i] - input[i-1]);
     }
-    auto it = mp.begin();
-    pair<int,int> pp = *it;
-    int ans = 0;
-    int xx = pp.second;
-    int full = n-xx;
-    ans += (full * pp.first);
-    ans += pp.first;
-    ans += (pp.first - 1) * (xx-1);
-    cout<<ans<<endl;
+
+    int cnt = 1;
+    for(int i = 0; i<diff.size(); i++){
+        diff[i]*= cnt;
+        cnt++;
+    }
+
+    for(int i = 1; i<diff.size(); i++){
+        diff[i] += diff[i-1];
+    }
+
+    print(diff);
+
+    int xx = lower_bound(diff.begin(), diff.end(), k) - diff.begin();
+    if(xx<n-1 and diff[xx] == k) xx++;
+
     
 
+    for(int i = 0; i<xx; i++){
+        if(xx<n) input[i] = input[xx];
+        else input[i] = input[n-1];
+    }
 
+    debug(xx);
 
+    if(xx-1 <n-1 and xx-1 >=0) k -= diff[xx-1];
+    else if(xx-1 == n-1) k -= diff[n-2];
+    
 
+    if(k>0){
+        int fill = k / (xx+1);
+        int rem = k % (xx+1);
+
+        for(int i = 0;i<=min(xx, n-1); i++){
+            input[i] += fill;
+        }
+
+        for(int i = 0; i < min(rem, n) ; i++){
+            input[i] ++;
+        }
+    }
+
+    
+    sort(input.begin(), input.end());
+    print(input);
+    int cnt2 = 0;
+    for(int i = 0; i<n; i++){
+        if(input[i] ==  input[0]) cnt2++;
+        else break;
+    }
+
+    debug(cnt2);
+
+    int ans = 0;
+
+    ans += ((n-cnt2) * input[0]);
+
+    ans += input[0];
+
+    ans += ((input[0]-1 ) * (cnt2-1));
+    cout<<ans<<endl;
 }
 /* logic ends */
 
