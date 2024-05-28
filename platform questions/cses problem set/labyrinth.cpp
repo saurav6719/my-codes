36f81e23fa76bf14fe3dff7f4c82c2bfe86b058d
Saurav Kumar <sauravkr485@gmail.com>
2024-05-29 03:48:51 +0530

    labyrinth

--- platform questions/cses problem set/labyrinth.cpp
+++ platform questions/cses problem set/labyrinth.cpp
@@ -1,7 +1,7 @@
 /*
   ------------------------------------------
  |                                        |
- |      Code Crafted by Saurav     |
+ |      Code Crafted by Saurav            |
  |                                        |
   ------------------------------------------
     \        ,     ,        /
@@ -15,7 +15,7 @@
 
 /* includes and all */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #ifndef ONLINE_JUDGE
 #define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
 #define print(v) do { \
@@ -36,97 +36,100 @@
 #define mx(a,b,c) max(a,max(b,c))
 using namespace std;
 
-/* write core logic here */
-int n;
-int m;
-vector<vector<int> > visited;
-string ans = "";
-void dfs(vector<vector<char> > &graph, int n, int m, int sr, int sc, string curr){
-    
-    if(graph[sr][sc] == 'B'){
-        if(ans.size() > 0){
-            if(curr.size() <= ans.size()){
-                ans = curr;
-            }
-        }  
-        else ans = curr;
-        return ;
-    }
+int n, m;
+vector<vector<bool>> visited;
+vector<vector<char>> direction;
+vector<vector<pair<int, int>>> parent;
+int dr = -1, dc = -1;
+
+void bfs(vector<vector<char>> &graph, int sr, int sc) {
+    queue<pair<int, int>> qu;
+    qu.push({sr, sc});
     visited[sr][sc] = true;
 
+    while (!qu.empty()) {
+        auto [r, c] = qu.front();
+        qu.pop();
 
-    if(sr+1 < n and visited[sr+1][sc] == false and ((graph[sr+1][sc] == '.') or (graph[sr+1][sc] == 'B'))){
-        dfs(graph, n, m,  sr+1, sc, curr + 'D');
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        vector<char> dirChars = {'D', 'U', 'R', 'L'};
 
+        for (int k = 0; k < 4; ++k) {
+            int nr = r + directions[k].first;
+            int nc = c + directions[k].second;
+
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && !visited[nr][nc]) {
+                visited[nr][nc] = true;
+                direction[nr][nc] = dirChars[k];
+                parent[nr][nc] = {r, c};
+
+                if (graph[nr][nc] == 'B') {
+                    dr = nr;
+                    dc = nc;
+                    return;
+                }
+                
+                if (graph[nr][nc] == '.') {
+                    qu.push({nr, nc});
+                }
+            }
+        }
     }
-
-    if(sc+1 < m and visited[sr][sc+1] == false and ((graph[sr][sc+1] == '.')  or (graph[sr][sc+1] == 'B'))){
-        dfs(graph, n, m,  sr, sc+1, curr + 'R');
-        
-    }
-
-    if(sr-1 >= 0 and visited[sr-1][sc] == false and ((graph[sr-1][sc] == '.') or (graph[sr-1][sc] == 'B'))){
-        dfs(graph, n, m,  sr-1, sc, curr + 'U');
-        
-    }
-
-    if(sc-1 >= 0 and visited[sr][sc-1] == false and ((graph[sr][sc-1] == '.')  or (graph[sr][sc-1] == 'B'))){
-        dfs(graph, n, m,  sr, sc-1, curr + 'L');
-        
-    }
-
-    visited[sr][sc] = false;
-    //curr.pop_back();
-
-
-    
 }
-void solve(){
-    
-    cin>>n>>m;
-    vector<vector<char> > input(n, vector<char> (m));
-    for(int i = 0; i<n; i++){
-        for(int j = 0 ; j<m; j++){
-            cin>>input[i][j];
+
+void solve() {
+    cin >> n >> m;
+    vector<vector<char>> input(n, vector<char>(m));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> input[i][j];
         }
     }
 
-    visited.resize(n+5, vector<int> (m+5, false));
-    int sr;
-    int sc;
+    visited.resize(n, vector<bool>(m, false));
+    direction.resize(n, vector<char>(m, ' '));
+    parent.resize(n, vector<pair<int, int>>(m, {-1, -1}));
 
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<m; j++){
-            if(input[i][j] == 'A'){
+    int sr = -1, sc = -1;
+    for (int i = 0; i < n && sr == -1; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (input[i][j] == 'A') {
                 sr = i;
                 sc = j;
                 break;
             }
         }
     }
-    dfs(input,n,m,sr,sc,"");
-    if(ans.size() > 0){
-        cout<<"YES"<<endl;
-        cout<<ans.size()<<endl;
-        cout<<ans<<endl;
+
+    bfs(input, sr, sc);
+
+    if (dr != -1) {
+        string ans;
+        int r = dr, c = dc;
+        while (!(r == sr && c == sc)) {
+            ans += direction[r][c];
+            auto [pr, pc] = parent[r][c];
+            r = pr;
+            c = pc;
+        }
+        reverse(ans.begin(), ans.end());
+        cout << "YES" << endl;
+        cout << ans.size() << endl;
+        cout << ans << endl;
+    } else {
+        cout << "NO" << endl;
     }
-    else cout<<"NO"<<endl;
-
 }
-/* logic ends */
 
-signed main(){
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     #ifndef ONLINE_JUDGE
-        freopen("Error.txt" , "w" , stderr);
+        freopen("Error.txt", "w", stderr);
     #endif
-    int t;
-    //cin>>t;
-    t = 1;
-    while(t--){
+    int t = 1;
+    while (t--) {
         solve();
     }
-return 0;
+    return 0;
 }
-
