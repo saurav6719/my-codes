6ac11f1858e79dd661c5199922005c5cbc6c3397
Saurav Kumar <sauravkr485@gmail.com>
2024-06-02 18:04:23 +0530

    xor

--- try2.cpp
+++ try2.cpp
@@ -1,49 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
 
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
 using namespace std;
 
-// Function to perform DFS and find the longest path to a leaf node
-void dfs(const vector<vector<int>>& tree, int node, int parent, int depth, int& maxDepth) {
-    // If it's a leaf node (and not the source node), check the depth
-    if (tree[node].size() == 1 && node != parent) {
-        if (depth > maxDepth) {
-            maxDepth = depth;
-        }
-    }
+/* write core logic here */
+void solve(){
+    cout<<(2 ^ 7 ^ 2)<<endl;
+}
+/* logic ends */
 
-    for (int neighbor : tree[node]) {
-        if (neighbor != parent) {
-            dfs(tree, neighbor, node, depth + 1, maxDepth);
-        }
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
     }
+return 0;
 }
 
-int main() {
-    int n; // Number of nodes
-    cout << "Enter number of nodes: ";
-    cin >> n;
-
-    // Adjust for 1-indexed graph by initializing tree with n+1 size
-    vector<vector<int>> tree(n + 1);
-    //cout << "Enter edges (u v) for the tree:" << endl;
-    for (int i = 0; i < n - 1; ++i) {
-        int u, v;
-        cin >> u >> v;
-        tree[u].push_back(v);
-        tree[v].push_back(u);
-    }
-
-    int source; // Source vertex
-    //cout << "Enter source vertex: ";
-    cin >> source;
-
-    // Perform DFS to find the longest path from the source to a leaf node
-    int maxDepth = -1;
-    dfs(tree, source, -1, 0, maxDepth);
-
-    cout << "The longest path from the source to a leaf node has length: " << maxDepth << endl;
-
-    return 0;
-}
