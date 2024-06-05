a73bf1bb046120af6e7ce9192a017d5ef3a72662
Saurav Kumar <sauravkr485@gmail.com>
2024-06-05 15:55:40 +0530

    jsakkjf

--- platform questions/codeforces/1338A.cpp
+++ platform questions/codeforces/1338A.cpp
@@ -37,18 +37,55 @@
 using namespace std;
 
 /* write core logic here */
-void solve(){
-    string str1;
-    cin>>str1;
-    string str2;
-    cin>>str2;
 
-    int ans = 0;
-    for(int i = 0; i<str1.size(); i++){
-        if(str1[i] != str2[i]) ans++;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
     }
 
+    int idx = -1;
+    vector<int> prf(40, 0);
+
+    for(int i = 0; i<40; i++){
+        if(i==0) {
+            prf[i] = 1;
+            continue;
+        }
+        prf[i] = ((1<<i) + prf[i-1]);
+    }
+
+    //print(prf);
+
+
+    for(int i = 1; i<n; i++){
+        if(input[i] < input[i-1]){
+            idx = i;
+            break;
+        }
+    }
+
+    if(idx==-1){
+        cout<<0<<endl;
+        return;
+    }
+    int ans = INT_MIN;
+    while(idx < n){
+        if(input[idx] >= input[idx-1]){
+            idx++;
+            continue;
+        }
+        int diff = abs(input[idx] - input[idx - 1]);
+        int curr = lower_bound(prf.begin(), prf.end(), diff) - prf.begin();
+        debug(curr);
+        ans = max(ans, curr+1);
+        input[idx] = input[idx-1];
+        idx++;
+    }
     cout<<ans<<endl;
+    
 }
 /* logic ends */
 
@@ -59,8 +96,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
-    t = 1;
+    cin>>t;
+    //t = 1;
     while(t--){
         solve();
     }
