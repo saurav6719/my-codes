974ebe61a09645446e6305c5d772a15553708e32
Saurav Kumar <sauravkr485@gmail.com>
2024-06-05 12:16:58 +0530

    ergehrf

--- platform questions/codeforces/1416A.cpp
+++ platform questions/codeforces/1416A.cpp
@@ -38,8 +38,56 @@ using namespace std;
 
 /* write core logic here */
 void solve(){
-    vector<int> v;
-    int arr = {66,70, }
+    int n;
+    cin>>n;
+    vector<int> input(n+1);
+    for(int i = 1; i<=n; i++){
+        cin>>input[i];
+    }
+
+    vector<vector<int> >store(n+5, vector<int> ());
+    for(int i = 0; i<n+5; i++){
+        store[i].push_back(0);
+    }
+
+    for(int i = 1; i<=n; i++){
+        int ele = input[i];
+        store[ele].push_back(i);
+    }
+
+    for(int i = 0; i<n+5; i++){
+        store[i].push_back(n+1);
+    }
+
+
+    vector<int> ans(n+1, -1);
+
+    vector<pair<int,int> > pp;
+
+    for(int i = 1 ; i<=n; i++){
+        if(store[i].size() == 2) continue;
+        int diff = INT_MIN;
+        for(int j = 1; j<store[i].size(); j++){
+            diff = max(store[i][j] - store[i][j-1] , diff);
+        }
+        pp.push_back({diff, i});
+    }
+
+    for(auto ele : pp){
+        int ff = ele.first;
+        int ss = ele.second;
+        for(int i = ff; i<=n; i++){
+            if(ans[i] != -1)break;
+            ans[i] = ss;
+        }
+    }
+
+    for(int i = 1; i<=n; i++){
+        cout<<ans[i]<<" ";
+    }
+
+    cout<<endl;
+
 }
 /* logic ends */
 
@@ -50,8 +98,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
-    t = 1;
+    cin>>t;
+    //t = 1;
     while(t--){
         solve();
     }
