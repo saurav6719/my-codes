68d300b4ce9cff292e7ef05b9f531e37bc02ff5b
Saurav Kumar <sauravkr485@gmail.com>
2024-07-20 16:01:33 +0530

    try

--- try.cpp
+++ try.cpp
@@ -30,16 +30,136 @@
 #define print(v)
 #endif
 #define endl "\n"
-#define int long long int
+// #define int long long int
 #define mod 1000000007
 #define mn(a,b,c) min(a,min(b,c))
 #define mx(a,b,c) max(a,max(b,c))
+#define ll long long int
 using namespace std;
 
 /* write core logic here */
-void solve(){
-    vector<int> v;
-    int arr = {66,70, }
+
+int binaryStringToInt(const string& binaryString) {
+    int result = 0;
+    int length = binaryString.length();
+    
+    for (int i = 0; i < length; ++i) {
+        if (binaryString[length - 1 - i] == '1') {
+            result += pow(2, i);
+        }
+    }
+    
+    return result;
+}
+
+
+
+string getbinary(int x){
+    bitset<64> b(x);
+    return b.to_string();
+}
+void solve() {
+    ll n;
+    cin >> n;
+    ll n_copy = n;
+    string str = getbinary(n);
+        ll bitCnt = 0;
+    while (n_copy > 0) {
+        if (n_copy & 1) bitCnt++;
+        n_copy >>= 1;
+    }
+
+    if(n==1){
+      cout<<1<<endl;
+      cout<<1<<endl;
+      return;
+    }
+
+    vector<vector<ll>> ans;
+    vector<ll> first;
+    ll s;
+    if (bitCnt % 2 == 1) {
+        s = 0;
+        for (ll i = 0; i < bitCnt; i++) {
+            first.push_back(s);
+            s = !s;
+        }
+    } else {
+        s = 0;
+        for (ll i = 0; i < bitCnt; i++) {
+            first.push_back(s);
+            s = !s;
+        }
+    }
+    ans.push_back(first);
+    ll pos = 1;
+    while (true) {
+        vector<ll> v = ans[pos - 1];
+        v.insert(v.begin(), 1);
+        while (v.size() > bitCnt) {
+            v.pop_back();
+        }
+        ans.push_back(v);
+        ll cnt = 0;
+        for(ll i=0; i<v.size(); i++){
+            if(v[i] == 1) cnt++;
+        }
+        if(cnt == bitCnt) break;
+        pos++;
+    }
+
+    // for(auto &it : ans){
+    //     print(it);
+    // }
+
+    cout << ans.size() << endl;
+
+    for(ll i=0; i<ans.size(); i++){
+        ll curr = 0;
+        ll num = n;
+        for(ll j = 0; j < 63; j++){
+            if((1 & (num >> j)) == 1){
+                num = (num & ans[i][curr]);
+                curr++; 
+            }
+        }
+    }
+
+
+    vector<int> final;
+
+    // print(str);
+
+    for(int i = 0; i<ans.size(); i++){
+      int curr = 0;
+      string s;
+      s.resize(64, '0');
+
+      for(int j = 0 ;j<64; j++){
+        if(str[j] == '1'){
+          s[j] = ans[i][curr] + '0';
+          curr++;
+        }
+      }
+
+      // print(s);
+
+      final.push_back(binaryStringToInt(s));
+
+    }
+
+    for(auto ele : final){
+      cout<<ele<<" ";
+
+
+    }
+
+    cout<<endl;
+
+
+
+
+
 }
 /* logic ends */
 
@@ -50,8 +170,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
-    t = 1;
+    cin>>t;
+    //t = 1;
     while(t--){
         solve();
     }
--- try2.cpp
+++ try2.cpp
@@ -1,3 +1,4 @@
+
 /*
   ------------------------------------------
  |                                        |
@@ -37,116 +38,100 @@
 using namespace std;
 
 /* write core logic here */
-string countOfAtoms(string formula) {
-        int n = formula.size();
-        vector<int> index(n,0);
 
-        vector<pair<int,int> > bracks;
-        int curr = 0;
-
-        stack<int> st;
-        int i = 0;
-
-        while(i<n){
-            if(isupper(formula[i])){
-                curr = i;
-                index[curr] = 1;
-            }
-            if(isdigit(formula[i]) and curr!=-1){
-                string xx = "";
-                while(i<n and isdigit(formula[i]) ){
-                    xx += formula[i];
-                    i++;
-                }
-                index[curr] = stoi(xx);
-                i--;
-            }
-
-            if(formula[i] == '('){
-                st.push(i);
-            }
-
-            if(formula[i] == ')'){
-                curr = -1;
-                int xx = st.top();
-                st.pop();
-                bracks.push_back({xx, i});
-            }
-            i++;
-        }
-
-        //print(index);
-
-        `
-        for(int i = 0; i<bracks.size(); i++){
-            pair<int,int> a = bracks[i];   
-            int start = a.first;
-            int end = a.second;
-            
-            int tomul ;
-            string xx ="";
-            int y = end+1;
-            if(!isdigit(formula[y])) continue;
-            while(y<n and isdigit(formula[y])){
-                xx += formula[y];
-                y++;
-            }
-
-            tomul = stoi(xx);
-            for(int j = start; j<end; j++){
-                index[j] *= tomul;
-            }
-        }
-
-        //print(index);
-
-
-        vector<string> mapping(n, "");
-        int curr2 = 0;
-        for(int i = 0; i<n; i++){
-            if(isupper(formula[i])){
-                curr2 = i;
-                mapping[curr2] += formula[i];
-            }
-            if(islower(formula[i])){
-                mapping[curr2] += formula[i];
-            }
-        }
-
-        //print(mapping);
-        map<string, int> mp;
-
-
-        for(int i =0 ;i<n; i++){
-            if(mapping[i] != ""){
-                //print(mapping[i]);
-                //debug(index[i]);
-                int xx = index[i];                
-                mp[mapping[i]] += xx;
-            }
-        }
-
-        // for(auto ele : mp){
-        //     cout<<ele.first<<ele.second<<endl;
-        // }
-        // cout<<endl;
-
-        string ans = "";
-        for(auto ele: mp){
-            ans += ele.first;
-
-            if(ele.second > 1) ans += to_string(ele.second);
-        }
-
-        
-
-        return ans;
+bool cmp(pair<int,int> &a, pair<int,int> &b){
+    return a.first > b.first;
 }
-
 void solve(){
-    string str;
-    cin>>str;
-    cout<<countOfAtoms(str);
+
+    vector<int> a;
+    int n ;
+    cin>>n;
+    a.resize(n);
+    for(int i =0; i<n; i++){
+        cin>>a[i];
+    }
+
+    stack<pair<int,int> > st;
+
+    stack<pair<int,int> > st2;
+
+
+    vector<int> next(n);
+    vector<int> prev(n);
+
+    
+
+    for(int i = 0; i<n; i++){
+        int curr = a[i];
+        if(st.empty()){
+            st.push({a[i] , 1});
+            prev[i] = 1;
+            continue;
+        }
+        int cnt = 1;
+        while(!st.empty() and st.top().first >= curr){
+            cnt += st.top().second;
+            st.pop();
+        }
+        st.push({curr, cnt});
+        prev[i] = cnt;
+    }
+
+ 
+
+    for(int i = n-1; i>=0; i--){
+        int curr = a[i];
+        if(st2.empty()){
+            st2.push({a[i] , 1});
+            next[i] = 1;
+            continue;
+        }
+        int cnt = 1;
+        while(!st2.empty() and st2.top().first >= curr){
+            cnt += st2.top().second;
+            st2.pop();
+        }
+        st2.push({curr, cnt});
+        next[i] = cnt;
+    }
+
+    print(prev);
+    print(next);
+    
+    map<int, int> st3;
+
+    vector<pair<int,int> > input;
+
+    for(int i = 0; i<n; i++){
+        input.push_back({a[i], i});
+    }
+
+    sort(input.begin(), input.end(), cmp);
+    int curr = 1;
+    for(int i = 0; i<n; i++){
+        int aa = input[i].first;
+        int bb = input[i].second;
+        int xx = prev[bb] + next[bb] - 1;
+
+        if(st3.count(xx)){
+            continue;
+        }
+
+        while(curr <= xx){
+            st3[curr] = aa;
+            curr++;
+        }
+    }
+
+    vector<int> final(n+1);
+    for(auto ele: st3){
+        final[ele.first] = ele.second;
+    }
+
+    print(final);
+
+
 }
 /* logic ends */
 
