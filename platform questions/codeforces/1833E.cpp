c4d85a03b47d86777f442cdcc025b991f8b6350f
Saurav Kumar <sauravkr485@gmail.com>
2024-07-26 18:19:55 +0530

    1833 E

---
+++ platform questions/codeforces/1833E.cpp
@@ -0,0 +1,139 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#define print2d(v) do { \
+                    cout << "vect-- starts" << endl; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "[" << " "; \
+                        for (int j = 0; j < v[i].size(); j++) { \
+                            cout << v[i][j] << " "; \
+                        } \
+                        cout << "]" << endl; \
+                    } \
+                    cout << "vect-- ends" << endl; \
+                } while(0)
+#define printmap(m) do { \
+                    cout << "map-- starts" << endl; \
+                    for (auto it = m.begin(); it != m.end(); ++it) { \
+                        cout << it->first << " -> " << it->second << endl; \
+                    } \
+                    cout << "map-- ends" << endl; \
+                } while(0)
+
+#define printpp(v) do { \
+                    cout << "vect--" << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "(" << v[i].first << ", " << v[i].second << ") "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#define print2d(v)
+#define printmap(m)
+#define printpp(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+#define pp pair<int,int>
+using namespace std;
+
+/* write core logic here */
+
+int cyclecnt;
+int n;
+bool cycle;
+vector<set<int> > graph;
+
+void dfs(int i, vector<int> &visited){
+    visited[i] = 1;
+    if(graph[i].size() == 1){
+        cycle = false;
+    }
+    for(auto ele :graph[i]){
+        if(visited[ele] == -1){
+            dfs(ele, visited);
+        }
+    }
+}
+void solve(){
+    cin>>n;
+
+    graph.clear();
+    graph.resize(n+5);
+    
+
+    for(int i = 1; i<=n; i++){
+        int ele;
+        cin>>ele;
+
+        graph[i].insert(ele);
+        graph[ele].insert(i);
+    } 
+
+
+    int cnt = 0;
+    cyclecnt = 0;
+    vector<int> visited(n+5, -1);
+    for(int i =1; i<=n; i++){
+        if(visited[i] == -1){
+            cnt++;
+            cycle = true;
+            dfs(i,visited);
+            if(cycle == true) cyclecnt++;
+        }
+    }
+
+
+    int mini = min(1+cyclecnt , cnt);
+    int maxi = cnt;
+    cout<<mini<<" "<<maxi<<endl;
+
+
+
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
