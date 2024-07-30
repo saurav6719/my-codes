95924ec1e82f859386c26279361572804181c08e
Saurav Kumar <sauravkr485@gmail.com>
2024-07-30 17:35:40 +0530

    1920 c

---
+++ platform questions/codeforces/1920C.cpp
@@ -0,0 +1,153 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#define print2d(v) do { \
+                    cout << "vect-- starts" << endl; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "[" << " "; \
+                        for (int j = 0; j < v[i].size(); j++) { \
+                            cout << v[i][j] << " "; \
+                        } \
+                        cout << "]" << endl; \
+                    } \
+                    cout << "vect-- ends" << endl; \
+                } while(0)
+#define printmap(m) do { \
+                    cout << "map-- starts" << endl; \
+                    for (auto it = m.begin(); it != m.end(); ++it) { \
+                        cout << it->first << " -> " << it->second << endl; \
+                    } \
+                    cout << "map-- ends" << endl; \
+                } while(0)
+
+#define printpp(v) do { \
+                    cout << "vect--" << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "(" << v[i].first << ", " << v[i].second << ") "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#define print2d(v)
+#define printmap(m)
+#define printpp(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+#define pp pair<int,int>
+using namespace std;
+
+/* write core logic here */
+
+std::vector<int> getAllDivisors(int n) {
+    std::vector<int> divisors;
+    for (int i = 1; i <= std::sqrt(n); ++i) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i != n / i) {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+    std::sort(divisors.begin(), divisors.end());
+    return divisors;
+}
+
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
+    }
+
+    int ans = 1;
+
+    vector<int> factors = getAllDivisors(n);
+
+    // int ele = 1;
+    int x = 1;
+    for(auto ele : factors){
+        debug(ele);
+        int parts = n/ele;
+        debug(parts);
+        int cmp = n/parts;
+        debug(cmp);
+        if(parts <= 1) continue;
+
+        int j = 0;
+        int gcd = abs(input[cmp] - input[j]);
+
+        debug(gcd);
+
+        while(j<cmp){
+            int i = j+cmp;
+            debug(input[i]);
+            gcd = __gcd(gcd, abs(input[i] - input[j]));
+
+            while(i<n){
+                if(i + cmp < n){
+                    gcd = __gcd(gcd, abs(input[i+cmp] - input[i]));
+                }
+                i+= cmp;
+                
+            }
+            j++;
+        }
+
+        debug(gcd);
+
+        if(gcd >= 2 or gcd == 0) ans++;
+    }
+
+    cout<<ans<<endl;
+
+
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    // t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
