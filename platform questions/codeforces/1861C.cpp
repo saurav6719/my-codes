23e0b9b938363f9505cc27ea361e66c8df6d6c2c
Saurav Kumar <sauravkr485@gmail.com>
2024-08-05 17:15:00 +0530

    1861 c

--- platform questions/codeforces/1861C.cpp
+++ platform questions/codeforces/1861C.cpp
@@ -67,50 +67,155 @@
 using namespace std;
 
 /* write core logic here */
+
+bool f(int l1, int r1, int l2, int r2){
+
+    if(l1 > r1) swap(l1, r1);
+    if(l2 > r2) swap(l2, r2);
+    debug(l1);
+    debug(r1);
+    debug(l2);
+    debug(r2);
+    return max(l1, l2) <= min(r1, r2);
+}
+void solvee(){
+    string str;
+    cin>>str;
+    cout<<str;
+}
+
 void solve(){
     string str;
     cin>>str;
+    vector<int> v;
+    set<int> zero;
+    set<int> one;
+
+    pp onne;
+    pp zerro;
     int n = str.size();
-    vector<int> plusidx;
-    vector<set<int> > v(n+5); 
+
+    if(str[0] == '0'){
+        cout<<"NO"<<endl;
+        return;
+    }
+
+    int zc = 0;
     for(int i = 0; i<n; i++){
+        if(str[i] == '0') zc++;
+    }
+
+    if(zc == 0){
+        cout<<"YES"<<endl;
+        return;
+    }
+
+    for(int i = 0; i<n; i++){
+        if(zero.size() == 1 and zero.count(0)){
+            zero.clear();
+        }
+        if(one.size() ==1 and one.count(0)){
+            one.clear();
+        }
         if(str[i] == '+'){
-            plusidx.push_back(i);
-            continue;
+            v.push_back(i);
         }
         if(str[i] == '-'){
-            plusidx.pop_back();
+            int xx = v.back();
+            int yy = v[0];
+            v.pop_back();
+            if(v.empty()){
+                zero.clear();
+                one.clear();
+                continue;
+            }
+            int newxx = v.back();
+            if(zero.count(xx) and one.count(xx)){
+                cout<<"NO"<<endl;
+                return;
+            }
+            if(zero.count(xx)){
+                zero.erase(xx);
+            }
+            if(one.count(xx)){
+                // cout<<i<<endl;
+                one.clear();
+                one.insert(newxx);
+                one.insert(yy);
+            }
         }
-        else{
-            v[plusidx.back()].insert(str[i] - '0');
+
+        if(str[i] == '1'){
+
+            // debug(*zero.begin());
+            // debug(*zero.rbegin());
+
+            if(v.size() == 0){
+                continue;
+            }
+
+            int xx = v.back();
+            int yy = v[0];
+
+            if(zero.size() == 0){
+                one.clear();
+                one.insert(xx);
+                one.insert(yy);
+                continue;
+            }
+
+            int yyy = *(zero.rbegin());
+            int xxx = *(zero.begin());
+            int l1 = xx;
+            int r1 = yy;
+            int l2 = xxx;
+            int r2 = yyy;
+
+            
+            if(f(l1,r1,l2,r2)){
+
+                cout<<"NO"<<endl;
+                return;
+            }
+
+            one.clear();
+            one.insert(xx);
+            one.insert(yy);
         }
-    } 
+        if(str[i] == '0'){
+            if(v.size() <= 1){
+                cout<<"NO"<<endl;
+                return;
+            }
 
-    for(int i = 0; i<n; i++){
-        if(v[i].size() > 1){
-            cout<<"NO"<<endl;
-            return;
+            int xx = v.back();
+            
+            
+            if(one.size() == 0){
+
+                zero.insert(xx);
+                continue;
+            }
+
+            int yyy = *(one.rbegin());
+            int xxx = *(one.begin());
+            int l1 = xx;
+            int r1 = xx;
+            int l2 = xxx;
+            int r2 = yyy;
+
+            
+
+            if(f(l1,r1,l2,r2)){
+                cout<<"NO"<<endl;
+                return;
+            }
+
+            zero.insert(xx);
         }
-    }
-
-
-
-
-
-    vector<int> z;
-    vector<int> o;
-    for(int i = 0; i<n;i++){
-        if(str[i] == '0') z.push_back(i);
-        if(str[i] == '1') o.push_back(i);
-    }
-
-    int curr = min(z[0], o[0]);
-    while(curr < n){
-        
     }
 
     cout<<"YES"<<endl;
-    return;
 }
 /* logic ends */
 
@@ -123,7 +228,11 @@ signed main(){
     int t;
     cin>>t;
     // t = 1;
-    while(t--){
+    for(int i = 1; i<=t ; i++){
+        // if(i==2638){
+        //     solvee();
+        // }
+        // else solve();
         solve();
     }
 return 0;
