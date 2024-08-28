16b5efb14c06a72d2f7ab8ecf6fb2cdb8662832a
Saurav Kumar <sauravkr485@gmail.com>
2024-08-28 17:02:50 +0530

    cses

---
+++ platform questions/codeforces/1279D.cpp
@@ -0,0 +1,128 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#define print2d(v) do { \
+                    cout << "vect-- starts" << endl; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "[" << " "; \
+                        for (int j = 0; j < v[i].size(); j++) { \
+                            cout << v[i][j] << " "; \
+                        } \
+                        cout << "]" << endl; \
+                    } \
+                    cout << "vect-- ends" << endl; \
+                } while(0)
+#define printmap(m) do { \
+                    cout << "map-- starts" << endl; \
+                    for (auto it = m.begin(); it != m.end(); ++it) { \
+                        cout << it->first << " -> " << it->second << endl; \
+                    } \
+                    cout << "map-- ends" << endl; \
+                } while(0)
+
+#define printpp(v) do { \
+                    cout << "vect--" << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "(" << v[i].first << ", " << v[i].second << ") "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#define print2d(v)
+#define printmap(m)
+#define printpp(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 998244353
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+#define pp pair<int,int>
+using namespace std;
+
+/* write core logic here */
+int modExp(int base, int exp) {
+    int result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return result;
+}
+
+// Function to find the modular inverse using modular exponentiation
+int modInverse(int b) {
+    return modExp(b, mod - 2, mod);
+}
+
+// Function to perform modular division
+int modDivision(int a, int b) {
+    a = a % mod;
+    int inv = modInverse(b, mod);
+    return (inv * a) % mod;
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<vector<int> > input(n);
+    map<int,int> mp;
+    for(int i = 0; i<n; i++){
+        int sz;
+        cin>>sz;
+        for(int j = 0; j<sz;  j++){
+            int ele;
+            cin>>ele;
+            input[i].push_back(ele);
+            mp[ele]++;
+        }
+    }
+
+
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    //cin>>t;
+    t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
