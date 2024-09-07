1929d060f4c058d64e89863df7b8b83eeb113bd1
Saurav Kumar <sauravkr485@gmail.com>
2024-09-07 10:39:46 +0530

    1684D.cpp

---
+++ platform questions/codeforces/1684D.cpp
@@ -0,0 +1,131 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#define print2d(v) do { \
+                    cout << "vect-- starts" << endl; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "[" << " "; \
+                        for (int j = 0; j < v[i].size(); j++) { \
+                            cout << v[i][j] << " "; \
+                        } \
+                        cout << "]" << endl; \
+                    } \
+                    cout << "vect-- ends" << endl; \
+                } while(0)
+#define printmap(m) do { \
+                    cout << "map-- starts" << endl; \
+                    for (auto it = m.begin(); it != m.end(); ++it) { \
+                        cout << it->first << " -> " << it->second << endl; \
+                    } \
+                    cout << "map-- ends" << endl; \
+                } while(0)
+
+#define printpp(v) do { \
+                    cout << "vect--" << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "(" << v[i].first << ", " << v[i].second << ") "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#define print2d(v)
+#define printmap(m)
+#define printpp(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+#define pp pair<int,int>
+using namespace std;
+
+/* write core logic here */
+bool cmp(pp &a, pp &b){
+    return a.first > b.first;
+}
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    vector<int> input(n);
+    for(int i = 0; i<n; i++){
+        cin>>input[i];
+    }
+
+    vector<pp> b(n);
+    for(int i = 0; i<n; i++){
+        b[i].first = input[i] - (n - i - 1);
+        b[i].second = i;
+    }
+
+    sort(b.begin(), b.end(), cmp);
+
+    vector<int> liyanhilia(n, 0);
+
+    for(int i = 0; i<k; i++){
+        liyanhilia[b[i].second] = 1;
+    }
+
+    
+    int ans = 0;
+
+    vector<int> prf(n,0);
+
+    for(int i = 1; i<n; i++){
+        if(liyanhilia[i-1] == 1){
+            prf[i] = prf[i-1] + 1;
+        }
+        else prf[i] = prf[i-1];
+    }
+
+    for(int i = 0; i<n; i++){
+        if(liyanhilia[i] == 0){
+            ans += input[i] + prf[i];
+        }
+    }
+
+    cout<<ans<<endl;
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
