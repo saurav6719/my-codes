f823d5fee060915ab5ffffc053a4a8c3536f5411
Saurav Kumar <sauravkr485@gmail.com>
2024-09-11 00:47:02 +0530

    stripe banking system

--- try2.cpp
+++ try2.cpp
@@ -67,112 +67,280 @@
 using namespace std;
 
 /* write core logic here */
+
+class Transaction{
+    public: 
+    map<string, set<string> > userbank;
+    set<string> username;
+    map<string, set<int> > userstimestamp;
+    set<string> banksname;
+    map<string ,map<int , int> > timestampbalance;
+    map<string, int> userscurrentbalance;
+    // set<int> 
+    
+
+    void initialise(vector<string> names, vector<int> startbalance, vector<vector<string> > banks){
+        int n = names.size();
+        for(int i = 0; i<n; i++){
+
+            string user = names[i];
+            userstimestamp[user].insert(0);
+            username.insert(user);
+            int startbb = startbalance[i];
+            userscurrentbalance[user] = startbb;
+            timestampbalance[user][0] = startbb;
+            for(int j = 0; j<banks[i].size(); j++){
+                userbank[user].insert(banks[i][j]);
+                banksname.insert(banks[i][j]);
+            }
+        }
+    }
+
+    string post(int timestamp, string sendersname, string recieversname, int amount){
+        if(banksname.count(sendersname) and banksname.count(recieversname)){
+            return "FAILURE";
+        }
+
+        if(banksname.count(sendersname) and userbank[recieversname].count(sendersname) == 0){
+            return "FAILURE";
+        }
+        if(banksname.count(recieversname) and userbank[sendersname].count(recieversname) == 0){
+            return "FAILURE";
+        }
+
+        if(banksname.count(sendersname) and username.count(sendersname)){
+            return "FAILURE";
+        }
+
+        if(banksname.count(recieversname) and username.count(recieversname)){
+            return "FAILURE";
+        }
+
+        
+
+        if(banksname.count(sendersname) == 0){
+            if(userscurrentbalance[sendersname] < amount ){
+                return "FAILURE";
+            }
+        }
+
+        if(username.count(sendersname)){
+
+            // cout<<"GHSUGIQ"<<endl;
+            userscurrentbalance[sendersname] -= amount;
+            timestampbalance[sendersname][timestamp] = userscurrentbalance[sendersname];
+            userstimestamp[sendersname].insert(timestamp);
+        }
+
+        if(username.count(recieversname)){
+            userscurrentbalance[recieversname] += amount;
+            timestampbalance[recieversname][timestamp] = userscurrentbalance[recieversname];
+            userstimestamp[recieversname].insert(timestamp);
+
+            // cout<<"HJKH"<<endl;
+
+            for(auto ele2 : userstimestamp[recieversname]){
+                debug(ele2);
+            }
+        }
+
+        return "SUCCESS";
+    }
+
+    string get (int timestamp, string currusername){
+
+        if(username.count(currusername) == 0){
+            return "FAILURE";
+        }
+
+        set<int> st = userstimestamp[currusername];
+        for(auto ele : st){
+            debug(ele);
+        }
+        auto  lb = userstimestamp[currusername].lower_bound(timestamp);
+        if(lb != userstimestamp[currusername].end() and *lb == timestamp) lb++;
+        lb--;
+        int time = *(lb);
+        
+        int money = timestampbalance[currusername][time];
+        return to_string(money) ;
+    }
+};
 void solve(){
     int n;
     cin>>n;
-    vector<int> input(n);
-    int maxi = INT_MIN;
+    int initcnt = 0;
+    vector<string> commands(n);
+
     for(int i = 0; i<n; i++){
-        cin>>input[i];
-        maxi = max(maxi , input[i]);
+        cin>>commands[i];
+        
     }
 
-    vector<int> newv(n);
-    bool done = false;
-    for(int i = 0; i<n; i++){
-        if(input[i] == maxi and done ){
-            newv[i] = maxi;
-            done = true;
+    print(commands[0]);
+
+    vector<string> ipg;
+
+    vector<string> names;
+    vector<int> startbalance;
+    vector<vector<string> > banks;
+
+
+
+    Transaction T;
+
+    // cout<<"YES"<<endl;
+
+    for(auto str : commands){
+        string cmd = "";
+
+        int i = 0;
+        while(str[i] != ','){
+            cmd += str[i];
+            i++;
+        }
+        i++;
+
+        print(cmd);
+
+        if(cmd == "INIT"){
+            ipg.push_back("INIT");
+            initcnt ++;
+            string name = "";
+            
+            while(i<str.size() and str[i] != ','){
+                name += str[i];
+                i++;
+            }
+
+            names.push_back(name);
+
+            i++;
+            string startb = "";
+
+            while(str[i] != ','){
+                startb += str[i];
+                i++;
+            }
+
+
+
+            i++;
+
+            int startbbb = stoi(startb);
+
+            startbalance.push_back(startbbb);
+
+            vector<string> userbank;
+            
+            while(i<str.size()){
+                int j = i;
+                string currbank ="";
+                while(j<str.size() and str[j] != ','){
+                    currbank += str[j];
+                    j++;
+                }
+                userbank.push_back(currbank);
+                i = j+1;
+            }
+
+            banks.push_back(userbank);
+        }
+
+
+
+        else if(cmd == "POST"){
+            if(ipg.back() == "INIT"){
+                T.initialise(names, startbalance, banks);
+            }
+            ipg.push_back("POST");
+
+            string timestamp = "";
+
+            while(i<str.size() and str[i] != ','){
+                timestamp += str[i];
+                i++;
+            }
+
+            print(timestamp);
+
+            int timestampint = stoi(timestamp);
+
+            i++;
+
+            string sendersname = "";
+
+            while(i<str.size() and str[i] != ','){
+                sendersname += str[i];
+                i++;
+            }
+
+            i++;
+
+            string recieversname = "";
+
+            while(i<str.size() and str[i] != ','){
+                recieversname += str[i];
+                i++;
+            }
+
+            print(sendersname);
+            print(recieversname);
+
+            i++;
+
+            string amount = "";
+
+            while(i<str.size() and str[i] != ','){
+                amount += str[i];
+                i++;
+            }
+
+            i++;
+
+            int amountint = stoi(amount);
+
+            string ans = T.post(timestampint,  sendersname,  recieversname, amountint);
+
+            cout<<ans<<endl;
+
         }
         else{
-            newv[i] = input[i];
+            if(ipg.back() == "INIT"){
+                T.initialise(names, startbalance, banks);
+            }
+            ipg.push_back("GET");
+
+            string timestamp = "";
+
+            while(i<str.size() and str[i] != ','){
+                timestamp += str[i];
+                i++;
+            }
+
+            int timestampint = stoi(timestamp);
+
+            i++;
+
+            string getuser = "";
+
+            while(i<str.size() and str[i] != ','){
+                getuser += str[i];
+                i++;
+            }
+
+            cout<<T.get(timestampint, getuser)<<endl;
         }
     }
 
-    int currodd = 1;
-    int curreven = 2;
 
-    for(int i = 0; i<n; i++){
-        if(input[i] % 2 != maxi % 2){
-            input[i]++;
-            currodd += 2;
-        }
+    for(auto ele4 : T.username){
+        print(ele4);
     }
 
-    debug(currodd);
-
-    for(int i = 0; i<n; i++){
-        int diff = maxi - input[i];
-        debug(diff);
-        int daysreq = diff /2;
-        curreven += (daysreq * 2);
+    for(auto ele3 : T.banksname){
+        print(ele3);
     }
-
-    int lasteven = curreven - 2;
-    debug(lasteven);
-
-    while(currodd + 2 < lasteven){
-        currodd += 4;
-        lasteven -= 2;
-    }
-
-
-    
-
-    int ans1 = max(currodd-2, lasteven);
-
-
-
-
-
-    int currodd2 = 3;
-    int curreven2 = 2;
-    int newmaxi = maxi+1;
-
-    for(int i =0; i<n; i++){
-        if(newv[i] == maxi){
-            newv[i] = newmaxi;
-            break;
-        }
-    }
-
-    for(int i = 0; i<n; i++){
-        if(newv[i] % 2 != newmaxi % 2){
-            newv[i]++;
-            currodd2 += 2;
-        }
-    }
-
-    // debug(currodd);
-
-    for(int i = 0; i<n; i++){
-        int diff = newmaxi - newv[i];
-        // debug(diff);
-        int daysreq = diff / 2;
-        curreven2 += (daysreq * 2);
-    }
-
-    int lasteven2 = curreven2 - 2;
-    // debug(lasteven);
-
-    debug(lasteven2);
-
-    while(currodd2 + 2 < lasteven2){
-        debug(currodd2);
-        debug(lasteven2);
-        currodd2 += 4;
-        lasteven2 -= 2;
-    }
-
-    int ans2 = max(currodd2-2, lasteven2);
-    debug(ans1);
-    debug(ans2);
-
-    cout<<min(ans1, ans2)<<endl;
-
-
-
-
 }
 /* logic ends */
 
@@ -183,8 +351,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    cin>>t;
-    //t = 1;
+    // cin>>t;
+    t = 1;
     while(t--){
         solve();
     }
