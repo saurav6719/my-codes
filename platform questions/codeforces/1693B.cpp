4e9416e205ee5f7f8fec178688f2b96de2af85b8
Saurav Kumar <sauravkr485@gmail.com>
2024-09-14 07:27:41 +0530

    1800 1

---
+++ platform questions/codeforces/1693B.cpp
@@ -0,0 +1,145 @@
+/*
+  ------------------------------------------
+ |                                        |
+ |      Code Crafted by Saurav     |
+ |                                        |
+  ------------------------------------------
+    \        ,     ,        /
+      \      |     |      /
+         \   \___/   /
+           \  -----  /
+             \_____/
+  
+  Happy coding! 
+*/
+
+/* includes and all */
+
+#include<bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define debug(x) cout<<"errr----  "<< #x <<" " <<x<<endl 
+#define print(v) do { \
+                    cout << "vect--" << #v << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << v[i] << " "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#define print2d(v) do { \
+                    cout << "vect-- starts" << endl; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "[" << " "; \
+                        for (int j = 0; j < v[i].size(); j++) { \
+                            cout << v[i][j] << " "; \
+                        } \
+                        cout << "]" << endl; \
+                    } \
+                    cout << "vect-- ends" << endl; \
+                } while(0)
+#define printmap(m) do { \
+                    cout << "map-- starts" << endl; \
+                    for (auto it = m.begin(); it != m.end(); ++it) { \
+                        cout << it->first << " -> " << it->second << endl; \
+                    } \
+                    cout << "map-- ends" << endl; \
+                } while(0)
+
+#define printpp(v) do { \
+                    cout << "vect--" << " = [ "; \
+                    for (int i = 0; i < v.size(); i++) { \
+                        cout << "(" << v[i].first << ", " << v[i].second << ") "; \
+                    } \
+                    cout << " ]" << endl; \
+                } while(0)
+#else
+#define debug(x)
+#define print(v)
+#define print2d(v)
+#define printmap(m)
+#define printpp(v)
+#endif
+#define endl "\n"
+#define int long long int
+#define mod 1000000007
+#define mn(a,b,c) min(a,min(b,c))
+#define mx(a,b,c) max(a,max(b,c))
+#define pp pair<int,int>
+using namespace std;
+
+/* write core logic here */
+int dfs(vector<vector<int> > &tree, vector<int> &visited, int i, vector<int> &l,vector<int> &r, int &ans){
+    int sum = 0;
+    visited[i] = 1;
+    debug(i);
+
+    for(auto ele : tree[i]){
+        // debug(ele);
+        if(visited[ele] == 0){
+            debug(ele);
+            sum += dfs(tree, visited, ele, l,r, ans);
+        }
+    }
+
+    if(i==2) debug(sum);
+    if(sum < l[i]){
+        debug(i);
+        ans ++;
+        return r[i];
+    }
+    else{
+        return min(sum, r[i]);
+    }
+}
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> parent(n+5);
+    vector<vector<int>>tree(n+5, vector<int> ());
+
+    for(int i = 2; i<=n; i++){
+        int par;
+        cin>>par;
+        tree[par].push_back(i);
+        tree[i].push_back(par);
+    }
+
+    print2d(tree);
+
+    int ans = 0;
+
+    vector<int> l(n+5);
+    vector<int> r(n+5);
+
+    for(int i = 1; i<=n; i++){
+        cin>>l[i]>>r[i];
+    }
+
+    print(l);
+    print(r);
+
+    vector<int> visited(n+5, 0);
+
+    dfs(tree, visited, 1, l,r,ans);
+
+    cout<<ans<<endl;
+
+
+
+}
+/* logic ends */
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt" , "w" , stderr);
+    #endif
+    int t;
+    cin>>t;
+    //t = 1;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+
