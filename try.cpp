c7224be7bc408f546dcb620695ff43e4d23fe9f0
Saurav Kumar <sauravkr485@gmail.com>
2024-10-20 21:58:01 +0530

    manacarp

--- try.cpp
+++ try.cpp
@@ -1,6 +1,6 @@
 /**
  *    author: Saurav
- *    created: 2024.10.20 15:06:56
+ *    created: 2024.10.20 19:16:04
  **/
 
 /* includes and all */
@@ -57,92 +57,142 @@
 using namespace std;
 
 /* write core logic here */
-struct pair_hash {
-    size_t operator()(const pp& p) const {
-        // A common way to combine hashes of two integers
-        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+class SegmentTree {
+private:
+    int n; // Size of the original array
+    vector<int> tree; // Segment tree array
+    vector<int> data; // Original array
+
+    // Helper function to build the tree
+    void buildTree(int pos, int left, int right) {
+        if (left == right) {
+            // Leaf node will have a single element
+            tree[pos] = data[left];
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        // Recursively build the left and right subtrees
+        buildTree(2 * pos + 1, left, mid);
+        buildTree(2 * pos + 2, mid + 1, right);
+        // Internal node will have the maximum of its children
+        tree[pos] = max(tree[2 * pos + 1], tree[2 * pos + 2]);
+    }
+
+    // Helper function to perform range maximum query
+    int rangeMaxQuery(int pos, int left, int right, int ql, int qr) const {
+        // If segment of this node is completely outside the query range
+        if (qr < left || ql > right)
+            return INT32_MIN; // Return minimum integer as it's irrelevant for max
+
+        // If segment of this node is completely inside the query range
+        if (ql <= left && right <= qr)
+            return tree[pos];
+
+        // If a part of this segment overlaps with the query range
+        int mid = left + (right - left) / 2;
+        int leftMax = rangeMaxQuery(2 * pos + 1, left, mid, ql, qr);
+        int rightMax = rangeMaxQuery(2 * pos + 2, mid + 1, right, ql, qr);
+        return max(leftMax, rightMax);
+    }
+
+    // Helper function to perform point update
+    void pointUpdate(int pos, int left, int right, int idx, int value) {
+        if (left == right) {
+            // Leaf node. Update element.
+            tree[pos] = value;
+            data[idx] = value;
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        if (idx <= mid)
+            pointUpdate(2 * pos + 1, left, mid, idx, value);
+        else
+            pointUpdate(2 * pos + 2, mid + 1, right, idx, value);
+        // Update current node based on children's values
+        tree[pos] = max(tree[2 * pos + 1], tree[2 * pos + 2]);
+    }
+
+public:
+    // Constructor to initialize the segment tree
+    SegmentTree(const vector<int>& input) {
+        data = input;
+        n = data.size();
+        // Allocate enough space for the segment tree
+        // The maximum size of the segment tree is 2*(2^ceil(log2(n))) - 1
+        int size = 1;
+        while (size < n) size <<= 1;
+        size = 2 * size - 1;
+        tree.assign(size, INT32_MIN);
+        buildTree(0, 0, n - 1);
+    }
+
+    // Function to perform range maximum query
+    int rangeMax(int ql, int qr) const {
+        if (ql < 0 || qr >= n || ql > qr) {
+            throw invalid_argument("Invalid query range");
+        }
+        return rangeMaxQuery(0, 0, n - 1, ql, qr);
+    }
+
+    // Function to perform point update
+    void update(int idx, int value) {
+        if (idx < 0 || idx >= n) {
+            throw invalid_argument("Index out of bounds");
+        }
+        pointUpdate(0, 0, n - 1, idx, value);
+    }
+
+    // Optional: Function to print the segment tree (for debugging)
+    void printTree() const {
+        for (int i = 0; i < tree.size(); ++i) {
+            cout << tree[i] << ' ';
+        }
+        cout << '\n';
     }
 };
 void solve(){
-    int n;
-    cin>>n;
-    multiset<int> ms;
-    unordered_map<int, vector<pp> > mp;
-    unordered_set<int> reaminingindex;
-    unordered_map<pp,int, pair_hash> stl;
-
-    for(int i = 0; i<n; i++){
-        int a,b;
-        cin>>a>>b;
-        mp[a].push_back({b,i}) ;
-        mp[b].push_back({a,i}) ;
-        stl[{a,b}]++;
-        ms.insert(a) ;
-        ms.insert(b) ;
-        reaminingindex.insert(i) ;
+    int n,m;
+    cin>>n>>m;
+    vector<int> input(n);
+    for(int i=0;i<n;i++){
+        cin>>input[i];
     }
 
-    for(auto ele : mp){
-        debug(ele.first);
-        for(auto x : ele.second){
-            debug(x.first);
-            debug(x.second);
-        }
+    vector<int> queries(m);
+    for(int i=0;i<m;i++){
+        cin>>queries[i];
     }
+    SegmentTree st(input);
 
-    for(auto & ele : mp){
-        sort(ele.second.begin(),ele.second.end()) ;
-    }
+    for(int i = 0; i<m; i++){
+        int val = queries[i];
+        // debug(val);
+        int lo = 0;
+        int hi = n-1;
+        int res = -1;
 
-    vector<int> ans;
-    while(reaminingindex.size() > 0){
-        int mini = *ms.begin() ;
-        debug(mini);
-        // ms.erase(ms.begin()) ;
-        for(auto ele : mp[mini]){
-            int idx = ele.second ;
-            debug(ele.first);
-            debug(idx);
-            if(reaminingindex.count(idx)){
-
-                if(stl.count({mini, ele.first})){
-                    // cout<<"YEE"<<endl;
-                    ans.push_back(mini);
-                    ans.push_back(ele.first) ;
-                    ms.erase(ms.find(mini)) ;
-                    ms.erase(ms.find(ele.first)) ;
-
-                    stl[{mini,ele.first}]-- ;
-                    if(stl[{mini,ele.first}] == 0){
-                        stl.erase(stl.find({mini,ele.first})) ;
-                        
-                    }
-                }
-
-                if(stl.count({ele.first, mini})){
-                    // cout<<"YEA"<<endl;
-                    ans.push_back(ele.first);
-                    ans.push_back(mini) ;
-                    ms.erase(ms.find(mini)) ;
-                    ms.erase(ms.find(ele.first)) ;
-                    stl[{ele.first,mini}]-- ;
-                    if(stl[{ele.first,mini}] == 0){
-                        stl.erase(stl.find({ele.first,mini})) ;
-                        
-                    }
-                }
-                
-                reaminingindex.erase(idx) ;
-
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(st.rangeMax(0, mid) >= val){
+                res = mid;
+                hi = mid - 1;
             }
-
+            else{
+                lo = mid + 1;
+            }
         }
-        
+
+        res = res + 1;
+
+        cout<<res<<" ";
+
+        if(res != 0){
+            st.update(res-1, input[res-1] - val);
+            input[res-1] = input[res-1] - val;
+        }
     }
-    for(auto x : ans){
-        cout<<x<<" ";
-    }
-    cout<<endl;
+
+
 
 
 }
@@ -155,8 +205,8 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    cin>>t;
-    //t = 1;
+    // cin>>t;
+    t = 1;
     while(t--){
         solve();
     }
