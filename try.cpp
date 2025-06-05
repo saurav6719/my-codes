9d0dfbd08ad8471c165ebaddf48294d737df772f
Saurav Kumar <sauravkr485@gmail.com>
2025-06-05 14:01:52 +0530

    virtual contests

--- try.cpp
+++ try.cpp
@@ -55,11 +55,11 @@
 #define print(v)
 #define print2d(v)
 #define printmap(m)
-#define printpp(v)
 #define printset(s)
+#define printpp(v)
 #endif
 #define endl "\n"
-#define MOD 1000000007
+#define MOD 998244353
 #define mod_add(a, b) (((a) % MOD + (b) % MOD) % MOD)
 #define mod_sub(a, b) ((((a) % MOD - (b) % MOD) + MOD) % MOD)
 #define mod_mul(a, b) (((1LL * (a) % MOD) * (b) % MOD) % MOD)
@@ -70,143 +70,35 @@
 using namespace std;
 
 /* write core logic here */
-struct soldier{
-    int cost;
-    int damage;
-    int health;
-};
-struct CustomCompare {
-    bool operator()(tuple<int, int, int> a, tuple<int, int, int> b) {
-        if (get<0>(a) != get<0>(b))
-            return get<0>(a) > get<0>(b); // ascending by first
-        if(get<1>(a)  != get<1>(b))
-            return get<1>(a) < get<1>(b);     // descending by second
-        return get<2> (a) > get<2> (b);
+int cntdivisors(int n){
+    int ans = 0;
+    for(int i = 1 ; i<=n; i++){
+        if(n%i == 0) ans++;
     }
-};
-
-bool sortcmp(soldier &a, soldier &b){
-    if(a.cost != b.cost){
-        return a.cost < b.cost;
-    }
-    if(a.damage * a.health != b.damage * b.health){
-        return a.damage * a.health > b.damage * b.health;
-    }
-    return a.health > b.health;
+    return ans;
 }
 void solve(){
-    int n,c;
-    cin>>n>>c;
+    int n;
+    cin>>n;
+    vector<int> dp(n+1, 0);
+    
+    int sum = 0;
 
-    vector<soldier> v(n);
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, CustomCompare> pq;
-
-    for(int i = 0; i<n; i++){
-        cin>>v[i].cost>>v[i].damage>>v[i].health;
-    }
-
-    sort(v.begin(), v.end(), sortcmp);
-
-    for(int i= 0; i<n; i++){
-        pq.push({v[i].cost , v[i].damage * v[i].health, i});
-        debug(i);
-        debug(v[i].cost);
-    }
-
-    int bnachuke = 0;
-
-    vector<pp> capacityofsoldiers;
-
-    while(!pq.empty()){
-        auto [cost, capacity, index] = pq.top();
-        pq.pop();
-
-        debug(cost);
-        debug(capacity);
-        debug(index);
-
-        if(capacity < bnachuke + 1){
-            // push next 
-            int currstrength = capacity;
-            int toadd = bnachuke + 2 - currstrength;
-            int unitsrequired = toadd / (v[index].damage * v[index].health);
-            if(toadd % (v[index].damage * v[index].health) != 0) unitsrequired++;
-
-            int nextcost = cost + (v[index].cost * unitsrequired);
-            if(nextcost > c) continue;
-            int nextcapacity = capacity + (v[index].damage * v[index].health * unitsrequired);
-            int nextindex = index;
-            pq.push({nextcost, nextcapacity, nextindex});
-            continue;
-        }
-        if(capacity == bnachuke + 1 and bnachuke != 0) {
-            // push next 
-            int currstrength = capacity;
-            int toadd = bnachuke + 2 - currstrength;
-            int unitsrequired = toadd / (v[index].damage * v[index].health);
-            if(toadd % (v[index].damage * v[index].health) != 0) unitsrequired++;
-
-            int nextcost = cost + (v[index].cost * unitsrequired);
-            if(nextcost > c) continue;
-            int nextcapacity = capacity + (v[index].damage * v[index].health * unitsrequired);
-            int nextindex = index;
-            pq.push({nextcost, nextcapacity, nextindex});
-            continue;
-        }
-
-        capacityofsoldiers.push_back({capacity - 1, cost});
-        bnachuke = capacity - 1;
-
+    for(int i =1; i<=n; i++){
         {
-            // push next 
-            // int nextcost = cost + v[index].cost;
-            // int nextcapacity = capacity + v[index].damage * v[index].health;
-            // int nextindex = index;
-            // if(nextcost <= c) pq.push({nextcost, nextcapacity, nextindex});
-
-            //
-            int currstrength = capacity;
-            int toadd = bnachuke + 2 - currstrength;
-            int unitsrequired = toadd / (v[index].damage * v[index].health);
-            if(toadd % (v[index].damage * v[index].health) != 0) unitsrequired++;
-
-            int nextcost = cost + (v[index].cost * unitsrequired);
-            if(nextcost > c) continue;
-            int nextcapacity = capacity + (v[index].damage * v[index].health * unitsrequired);
-            int nextindex = index;
-            pq.push({nextcost, nextcapacity, nextindex});
+            int curr = i;
+            while(curr <= n){
+                dp[curr]++;
+                curr += i;
+            }
         }
-    }
-    printpp(capacityofsoldiers);
-    vector<int> temp;
-    for(auto ele : capacityofsoldiers){
-        temp.push_back(ele.first);
+        dp[i] += sum;
+        dp[i] %= MOD;
+        sum += dp[i];
+        sum %= MOD;
     }
 
-    int m;
-    cin>>m;
-    while(m--){
-        int damage;
-        int health;
-        cin>>damage>>health;
-
-        if(temp.empty()){
-            cout<<-1<<" ";
-            continue;
-        }
-
-        if((damage*health) > temp.back()){
-            cout<<-1<<" ";
-            continue;
-        }
-
-        int strength = damage * health;
-
-        int lb = lower_bound(temp.begin(), temp.end(), strength) - temp.begin();
-
-        cout<<capacityofsoldiers[lb].second<< " ";
-    }
-    cout<<endl;
+    cout<<dp[n];
 }
 /* logic ends */
 
@@ -217,7 +109,7 @@ signed main(){
         freopen("Error.txt" , "w" , stderr);
     #endif
     int t;
-    //cin>>t;
+    // cin>>t;
     t = 1;
     while(t--){
         solve();
